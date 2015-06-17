e9d2f2879b310e9d1e913b734ef37e2a4caf69e3
Venelin Valkov <venelin@curiousily.com>
2015-06-17 18:53:45 +0300

    Cleanup

--- app/src/main/java/com/curiousily/ipoli/AnalyticsConstants.java
+++
@@ -1,9 +0,0 @@
-package com.curiousily.ipoli;
-
-/**
- * Created by Venelin Valkov <venelin@curiousily.com>
- * on 6/16/15.
- */
-public interface AnalyticsConstants {
-    String TRACKING_CODE = "UA-63986975-2";
-}
