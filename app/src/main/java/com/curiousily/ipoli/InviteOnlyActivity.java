70fd5af1798e994e396b719c0c653c07a6b276af
Venelin Valkov <venelin@curiousily.com>
2015-09-11 22:27:17 +0300

    Randomly invite people

--- app/src/main/java/com/curiousily/ipoli/InviteOnlyActivity.java
+++ app/src/main/java/com/curiousily/ipoli/InviteOnlyActivity.java
@@ -1,13 +1,19 @@
 package com.curiousily.ipoli;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+
+import com.curiousily.ipoli.schedule.DailyScheduleActivity;
+
+import java.util.Random;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,6 +25,25 @@ public class InviteOnlyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_only);
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.getBoolean("is_invited", false)) {
+            startMainActivity();
+            return;
+        }
+
+        Random random = new Random();
+        int r = random.nextInt(100);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        boolean isInvited = r < 20;
+        editor.putBoolean("is_invited", isInvited);
+        editor.apply();
+
+        if (isInvited) {
+            startMainActivity();
+            return;
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -26,6 +51,11 @@ public class InviteOnlyActivity extends AppCompatActivity {
             window.setStatusBarColor(getResources().getColor(R.color.md_blue_700));
         }
         ButterKnife.bind(this);
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(this, DailyScheduleActivity.class));
+        finish();
     }
 
     @OnClick(R.id.invite_button)
