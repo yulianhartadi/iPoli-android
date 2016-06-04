133d09a4d9fd12c16186336a45ae5f003f7d95ff
Venelin Valkov <venelin@curiousily.com>
2016-06-04 17:17:20 +0300

    Replace XP LineChart with BarChart

--- app/src/main/java/io/ipoli/android/player/fragments/GrowthFragment.java
+++ app/src/main/java/io/ipoli/android/player/fragments/GrowthFragment.java
@@ -9,17 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.ValueFormatter;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.squareup.otto.Bus;
 
@@ -49,7 +51,7 @@ public class GrowthFragment extends BaseFragment {
     PieChart timeSpentChart;
 
     @BindView(R.id.experience_chart)
-    LineChart experienceChart;
+    BarChart experienceChart;
 
     private Unbinder unbinder;
 
@@ -72,139 +74,239 @@ public class GrowthFragment extends BaseFragment {
         App.getAppComponent(getContext()).inject(this);
         setUpTimeSpentChart();
 
-        LineData data = getData(7, 5);
-//        data.setValueTextColor(getColor(R.color.md_blue_500));
+//        LineData data = getData(7, 5);
+////        data.setValueTextColor(getColor(R.color.md_blue_500));
+//
+//        // add some transparency to the color with "& 0x90FFFFFF"
+//        setupChart(experienceChart, data);
 
-        // add some transparency to the color with "& 0x90FFFFFF"
-        setupChart(experienceChart, data);
+        experienceChart.setDrawBarShadow(false);
+        experienceChart.setDrawValueAboveBar(true);
+
+        experienceChart.setDescription("");
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        experienceChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        experienceChart.setPinchZoom(false);
+
+        experienceChart.setDrawGridBackground(false);
+        experienceChart.setExtraOffsets(5, 10, 5, 5);
+        // experienceChart.setDrawYLabels(false);
+
+        XAxis xAxis = experienceChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setTextColor(getColor(R.color.md_dark_text_54));
+        xAxis.setTextSize(10f);
+        xAxis.setSpaceBetweenLabels(2);
+
+        YAxis leftAxis = experienceChart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setTextColor(getColor(R.color.md_dark_text_54));
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setTextSize(10f);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = experienceChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setTextColor(getColor(R.color.md_dark_text_54));
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setTextSize(10f);
+        rightAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+
+        Legend l = experienceChart.getLegend();
+//        l.setEnabled(false);
+        l.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
+        l.setForm(Legend.LegendForm.SQUARE);
+        l.setFormSize(9f);
+        l.setTextSize(11f);
+        l.setXEntrySpace(4f);
+        // l.setExtra(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+        // "def", "ghj", "ikl", "mno" });
+        // l.setCustom(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+        // "def", "ghj", "ikl", "mno" });
+
+        setDataBar(7, 200);
 
 
         return view;
     }
 
-    private void setupChart(LineChart chart, LineData data) {
-
-//        ((LineDataSet) data.getDataSetByIndex(0)).setCircleColorHole(color);
-
-        // no description text
-        chart.setDescription("");
-        chart.setNoDataTextDescription("You need to provide data for the chart.");
-
-        // mChart.setDrawHorizontalGrid(false);
-        //
-        // enable / disable grid background
-//        chart.setDrawGridBackground(false);
-//        chart.getRenderer().getGridPaint().setGridColor(Color.WHITE & 0x70FFFFFF);
-
-        // enable touch gestures
-        chart.setTouchEnabled(true);
-
-        // enable scaling and dragging
-        chart.setDragEnabled(true);
-        chart.setScaleEnabled(true);
-
-        // if disabled, scaling can be done on x- and y-axis separately
-        chart.setPinchZoom(false);
-
-//        chart.setBackgroundColor(color);
-
-        // set custom chart offsets (automatic offset calculation is hereby disabled)
-        chart.setExtraOffsets(5, 10, 5, 5);
-
-        // add data
-        chart.setData(data);
-
-        // get the legend (only possible after setting data)
-        Legend l = chart.getLegend();
-//        l.setEnabled(false);
-//        chart.setDrawGridBackground(false);
+//    private void setupChart(LineChart chart, LineData data) {
 //
-        chart.getAxisLeft().setEnabled(false);
-        chart.getXAxis().setDrawGridLines(false);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.TOP);
-        chart.getXAxis().setAxisLineColor(getColor(R.color.md_dark_text_54));
-        chart.getXAxis().setTextColor(getColor(R.color.md_dark_text_54));
-        chart.getXAxis().setDrawAxisLine(false);
-        chart.setDrawBorders(true);
-        chart.setBorderWidth(0.5f);
-        chart.setBorderColor(getColor(R.color.md_dark_text_26));
-//        chart.getAxisLeft().setSpaceTop(40);
-//        chart.getAxisLeft().setSpaceBottom(40);
-        chart.getAxisRight().setDrawGridLines(false);
-        chart.getAxisRight().setDrawAxisLine(false);
-        chart.getAxisRight().setAxisLineColor(getColor(R.color.md_dark_text_54));
-        chart.getAxisRight().setTextColor(getColor(R.color.md_dark_text_54));
-//        chart.getAxisRight().setDrawLabels(false);
+////        ((LineDataSet) data.getDataSetByIndex(0)).setCircleColorHole(color);
+//
+//        // no description text
+//        chart.setDescription("");
+//        chart.setNoDataTextDescription("You need to provide data for the chart.");
+//
+//        // experienceChart.setDrawHorizontalGrid(false);
+//        //
+//        // enable / disable grid background
+////        chart.setDrawGridBackground(false);
+////        chart.getRenderer().getGridPaint().setGridColor(Color.WHITE & 0x70FFFFFF);
+//
+//        // enable touch gestures
+//        chart.setTouchEnabled(true);
+//
+//        // enable scaling and dragging
+//        chart.setDragEnabled(true);
+//        chart.setScaleEnabled(true);
+//
+//        // if disabled, scaling can be done on x- and y-axis separately
+//        chart.setPinchZoom(false);
+//
+////        chart.setBackgroundColor(color);
+//
+//        // set custom chart offsets (automatic offset calculation is hereby disabled)
+//        chart.setExtraOffsets(5, 10, 5, 5);
+//
+//        // add data
+//        chart.setData(data);
+//
+//        // get the legend (only possible after setting data)
+//        Legend l = chart.getLegend();
+////        l.setEnabled(false);
+////        chart.setDrawGridBackground(false);
+////
+//        chart.getAxisLeft().setEnabled(false);
+//        chart.getXAxis().setDrawGridLines(false);
+//        chart.getXAxis().setPosition(XAxis.XAxisPosition.TOP);
+//        chart.getXAxis().setAxisLineColor(getColor(R.color.md_dark_text_54));
+//        chart.getXAxis().setTextColor(getColor(R.color.md_dark_text_54));
+//        chart.getXAxis().setDrawAxisLine(false);
+//        chart.setDrawBorders(true);
+//        chart.setBorderWidth(0.5f);
+//        chart.setBorderColor(getColor(R.color.md_dark_text_26));
+////        chart.getAxisLeft().setSpaceTop(40);
+////        chart.getAxisLeft().setSpaceBottom(40);
+//        chart.getAxisRight().setDrawGridLines(false);
+//        chart.getAxisRight().setDrawAxisLine(false);
+//        chart.getAxisRight().setAxisLineColor(getColor(R.color.md_dark_text_54));
+//        chart.getAxisRight().setTextColor(getColor(R.color.md_dark_text_54));
+////        chart.getAxisRight().setDrawLabels(false);
+//
+////        chart.getXAxis().setEnabled(false);
+//
+//        // animate calls invalidate()...
+//        chart.animateX(getResources().getInteger(android.R.integer.config_longAnimTime));
+//    }
 
-//        chart.getXAxis().setEnabled(false);
-
-        // animate calls invalidate()...
-        chart.animateX(getResources().getInteger(android.R.integer.config_longAnimTime));
-    }
-
-    private LineData getData(int count, float range) {
+    private void setDataBar(int count, float range) {
 
         ArrayList<String> xVals = new ArrayList<String>();
         for (int i = 0; i < count; i++) {
             xVals.add(String.valueOf(i + 1));
         }
 
-        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
 
-        float val = 0;
         for (int i = 0; i < count; i++) {
-            val += (float) (Math.random() * range * 10);
-            yVals.add(new Entry(val, i));
+            float mult = (range + 1);
+            int val = (int) (Math.random() * mult);
+            yVals1.add(new BarEntry(val, i));
         }
 
-        // create a dataset and give it a type
-        LineDataSet set1 = new LineDataSet(yVals, "Experience");
+        BarDataSet set1;
 
-        set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-//        set1.setLineWidth(1.5f);
-//        set1.setDrawCircles(false);
-//        set1.setCircleRadius(2f);
-//        set1.setCircleHoleRadius(0.5f);
-        set1.setCircleRadius(2f);
-        set1.setDrawCircleHole(false);
-        set1.setColor(getColor(R.color.md_blue_300));
-        set1.setCircleColor(getColor(R.color.md_blue_500));
-        set1.setHighLightColor(Color.BLACK);
-        set1.setFillColor(getColor(R.color.md_blue_100));
-        set1.setDrawFilled(true);
-        set1.setDrawValues(false);
+//        if (experienceChart.getData() != null &&
+//                experienceChart.getData().getDataSetCount() > 0) {
+//            set1 = (BarDataSet)experienceChart.getData().getDataSetByIndex(0);
+//            set1.setYVals(yVals1);
+//            experienceChart.getData().setXVals(xVals);
+//            experienceChart.getData().notifyDataChanged();
+//            experienceChart.notifyDataSetChanged();
+//        } else {
+        set1 = new BarDataSet(yVals1, "Experience gain per day");
+        set1.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                return String.valueOf(Math.round(value));
+            }
+        });
+        set1.setBarSpacePercent(35f);
+        set1.setColors(new int[]{getColor(R.color.md_blue_300)});
 
-        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        dataSets.add(set1);
 
-        val = 0;
-        for (int i = 0; i < count; i++) {
-            val += (float) (Math.random() * range * 4);
-            yVals2.add(new Entry(val, i));
-        }
+        BarData data = new BarData(xVals, dataSets);
+        data.setValueTextSize(12f);
+        data.setValueTextColor(getColor(R.color.md_dark_text_87));
 
-        // create a dataset and give it a type
-        LineDataSet set2 = new LineDataSet(yVals2, "Coins");
-
-//        set2.setLineWidth(3f);
-        set2.setCircleRadius(2f);
-        set2.setDrawCircleHole(false);
-        set2.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-        set2.setColor(getColor(R.color.md_red_300));
-        set2.setCircleColor(getColor(R.color.md_red_500));
-        set2.setHighLightColor(Color.BLACK);
-        set2.setDrawFilled(true);
-//        set2.setDrawCircles(false);
-        set2.setFillColor(getColor(R.color.md_red_100));
-        set2.setDrawValues(false);
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
-        dataSets.add(set1); // add the datasets
-        dataSets.add(set2); // add the datasets
-
-        // create a data object with the datasets
-
-        return new LineData(xVals, dataSets);
+        experienceChart.setData(data);
+        experienceChart.animateY(getResources().getInteger(android.R.integer.config_longAnimTime));
     }
+
+//    private LineData getData(int count, float range) {
+//
+//        ArrayList<String> xVals = new ArrayList<String>();
+//        for (int i = 0; i < count; i++) {
+//            xVals.add(String.valueOf(i + 1));
+//        }
+//
+//        ArrayList<Entry> yVals = new ArrayList<Entry>();
+//
+//        float val = 0;
+//        for (int i = 0; i < count; i++) {
+//            val += (float) (Math.random() * range * 10);
+//            yVals.add(new Entry(val, i));
+//        }
+//
+//        // create a dataset and give it a type
+//        LineDataSet set1 = new LineDataSet(yVals, "Experience");
+//
+//        set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+////        set1.setLineWidth(1.5f);
+////        set1.setDrawCircles(false);
+////        set1.setCircleRadius(2f);
+////        set1.setCircleHoleRadius(0.5f);
+//        set1.setCircleRadius(2f);
+//        set1.setDrawCircleHole(false);
+//        set1.setColor(getColor(R.color.md_blue_300));
+//        set1.setCircleColor(getColor(R.color.md_blue_500));
+//        set1.setHighLightColor(Color.BLACK);
+//        set1.setFillColor(getColor(R.color.md_blue_100));
+//        set1.setDrawFilled(true);
+//        set1.setDrawValues(false);
+//
+//        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+//
+//        val = 0;
+//        for (int i = 0; i < count; i++) {
+//            val += (float) (Math.random() * range * 4);
+//            yVals2.add(new Entry(val, i));
+//        }
+//
+//        // create a dataset and give it a type
+//        LineDataSet set2 = new LineDataSet(yVals2, "Coins");
+//
+////        set2.setLineWidth(3f);
+//        set2.setCircleRadius(2f);
+//        set2.setDrawCircleHole(false);
+//        set2.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//        set2.setColor(getColor(R.color.md_red_300));
+//        set2.setCircleColor(getColor(R.color.md_red_500));
+//        set2.setHighLightColor(Color.BLACK);
+//        set2.setDrawFilled(true);
+////        set2.setDrawCircles(false);
+//        set2.setFillColor(getColor(R.color.md_red_100));
+//        set2.setDrawValues(false);
+//
+//        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+//        dataSets.add(set1); // add the datasets
+//        dataSets.add(set2); // add the datasets
+//
+//        // create a data object with the datasets
+//
+//        return new LineData(xVals, dataSets);
+//    }
 
     private void setUpTimeSpentChart() {
         //        timeSpentChart.setUsePercentValues(true);
@@ -224,13 +326,23 @@ public class GrowthFragment extends BaseFragment {
 
         timeSpentChart.setDrawCenterText(true);
 
+        String centerText = "XP: 2565\nCoins: 450";
+        timeSpentChart.setCenterText(centerText);
+        timeSpentChart.setCenterTextColor(getColor(R.color.md_dark_text_87));
+        timeSpentChart.setCenterTextSize(12f);
+//        centerTextLayout = new StaticLayout(centerText, 0, centerText.length(),
+//                mCenterTextPaint,
+//                (int)Math.max(Math.ceil(mCenterTextLastBounds.width()), 1.f),
+//                Layout.Alignment.ALIGN_NORMAL, 1.f, 0.f, false);
+
+
         timeSpentChart.setRotationAngle(0);
         // enable rotation of the chart by touch
         timeSpentChart.setRotationEnabled(true);
         timeSpentChart.setHighlightPerTapEnabled(true);
         timeSpentChart.getLegend().setEnabled(false);
         // add a selection listener
-        setData(5, 100);
+        setData(5, 30);
 
         timeSpentChart.animateY(getResources().getInteger(android.R.integer.config_longAnimTime), Easing.EasingOption.EaseInOutQuad);
 //        timeSpentChart.spin(getResources().getInteger(android.R.integer.config_mediumAnimTime), 0, 360, Easing.EasingOption.EaseInQuad);
@@ -327,6 +439,6 @@ public class GrowthFragment extends BaseFragment {
 
     @Override
     protected void showHelpDialog() {
-        HelpDialog.newInstance(R.layout.fragment_help_dialog_overview, R.string.help_dialog_overview_title, "overview").show(getActivity().getSupportFragmentManager());
+        HelpDialog.newInstance(R.layout.fragment_help_dialog_growth, R.string.help_dialog_growth_title, "growth").show(getActivity().getSupportFragmentManager());
     }
 }
