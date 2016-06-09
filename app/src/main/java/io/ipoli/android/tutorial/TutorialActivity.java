291f96d5b6b198f0ca5608c8d9424df17ce2caa4
Venelin Valkov <venelin@curiousily.com>
2016-06-09 15:15:07 +0300

    Hide done button on click

--- app/src/main/java/io/ipoli/android/tutorial/TutorialActivity.java
+++ app/src/main/java/io/ipoli/android/tutorial/TutorialActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -105,6 +106,7 @@ public class TutorialActivity extends AppIntro2 {
 
     @Override
     public void onDonePressed() {
+        doneButton.setVisibility(View.GONE);
         List<Quest> selectedQuests = pickQuestsFragment.getSelectedQuests();
         List<RepeatingQuest> selectedRepeatingQuests = pickRepeatingQuestsFragment.getSelectedQuests();
 
