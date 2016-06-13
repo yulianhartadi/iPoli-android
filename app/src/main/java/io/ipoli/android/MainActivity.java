284dd3b1ebab35d42819aae1c7cb49855b900c46
Venelin Valkov <venelin@curiousily.com>
2016-06-14 00:30:45 +0300

    Pick daily/weekly/monthly repeating pattern

--- app/src/main/java/io/ipoli/android/MainActivity.java
+++ app/src/main/java/io/ipoli/android/MainActivity.java
@@ -76,6 +76,7 @@ import io.ipoli.android.quest.fragments.OverviewFragment;
 import io.ipoli.android.quest.fragments.RepeatingQuestListFragment;
 import io.ipoli.android.quest.persistence.QuestPersistenceService;
 import io.ipoli.android.quest.persistence.RealmQuestPersistenceService;
+import io.ipoli.android.quest.ui.dialogs.RecurrencePickerFragment;
 import io.ipoli.android.reward.fragments.RewardListFragment;
 import io.ipoli.android.tutorial.TutorialActivity;
 import io.ipoli.android.tutorial.events.ShowTutorialEvent;
@@ -151,6 +152,8 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         };
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         updatePlayerInDrawer();
+
+        new RecurrencePickerFragment().show(getSupportFragmentManager());
     }
 
     @Override
--- app/src/main/java/io/ipoli/android/quest/ui/dialogs/RecurrencePickerFragment.java
+++ app/src/main/java/io/ipoli/android/quest/ui/dialogs/RecurrencePickerFragment.java
@@ -7,12 +7,25 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.squareup.otto.Bus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemSelected;
+import butterknife.Unbinder;
 import io.ipoli.android.R;
 import io.ipoli.android.app.App;
 
@@ -20,12 +33,29 @@ import io.ipoli.android.app.App;
  * Created by Venelin Valkov <venelin@curiousily.com>
  * on 6/13/16.
  */
-public class RecurrencePickerFragment extends DialogFragment {
+public class RecurrencePickerFragment extends DialogFragment implements AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "recurrence-picker-dialog";
+    public static final int FREQUENCY_DAILY = 0;
+    public static final int FREQUENCY_WEEKLY = 1;
+    public static final int FREQUENCY_MONTHLY = 2;
 
     @Inject
     Bus eventBus;
+
+    @BindView(R.id.recurrence_frequency)
+    Spinner recurrenceFrequency;
+
+    @BindView(R.id.day_of_week_container)
+    ViewGroup dayOfWeekContainer;
+
+    @BindView(R.id.day_of_month_container)
+    ViewGroup dayOfMonthContainer;
+
+    @BindView(R.id.day_of_month)
+    Spinner dayOfMonth;
+
+    private Unbinder unbinder;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,20 +69,68 @@ public class RecurrencePickerFragment extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(inflater.inflate(R.layout.fragment_recurrence_picker, null))
+        View view = inflater.inflate(R.layout.fragment_recurrence_picker, null);
+        unbinder = ButterKnife.bind(this, view);
+
+        recurrenceFrequency.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, new String[]{"Daily", "Weekly", "Monthly"}));
+        recurrenceFrequency.setSelection(0, false);
+        recurrenceFrequency.setOnItemSelectedListener(this);
+
+        List<String> daysOfMonth = new ArrayList<>();
+        for(int i = 1; i <= 31; i++) {
+            daysOfMonth.add(String.valueOf(i));
+        }
+        dayOfMonth.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, daysOfMonth));
+
+        builder.setView(view)
                 .setIcon(R.drawable.logo)
                 .setTitle("Pick repeating pattern")
-                .setPositiveButton(getString(R.string.help_dialog_ok), (dialog, which) -> {
+                .setPositiveButton(getString(R.string.done), (dialog, which) -> {
 
                 })
-                .setNeutralButton(getString(R.string.help_dialog_more_help), (dialog, which) -> {
+                .setNeutralButton(getString(R.string.do_not_repeat), (dialog, which) -> {
                 });
-        return builder.create();
 
+
+        return builder.create();
+    }
+
+    @Override
+    public void onDestroyView() {
+        unbinder.unbind();
+        super.onDestroyView();
     }
 
     public void show(FragmentManager fragmentManager) {
         show(fragmentManager, TAG);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case FREQUENCY_DAILY:
+                dayOfWeekContainer.setVisibility(View.GONE);
+                dayOfMonthContainer.setVisibility(View.GONE);
+                break;
+            case FREQUENCY_WEEKLY:
+                dayOfWeekContainer.setVisibility(View.VISIBLE);
+                dayOfMonthContainer.setVisibility(View.GONE);
+                break;
+            case FREQUENCY_MONTHLY:
+                dayOfWeekContainer.setVisibility(View.GONE);
+                dayOfMonthContainer.setVisibility(View.VISIBLE);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @OnItemSelected(R.id.day_of_month)
+    public void onDayOfMonthSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("DayOfMonthSel", position + " ");
+    }
 }
