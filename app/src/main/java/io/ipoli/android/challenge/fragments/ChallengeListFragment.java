59b34272da68619ae19611adc869ff2a7c8b75a5
Polina Zhelyazkova <poly_vjk@abv.bg>
2016-06-20 14:37:46 +0300

    Empty view for challenges list

--- app/src/main/java/io/ipoli/android/challenge/fragments/ChallengeListFragment.java
+++ app/src/main/java/io/ipoli/android/challenge/fragments/ChallengeListFragment.java
@@ -1,10 +1,11 @@
 package io.ipoli.android.challenge.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +20,20 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import io.ipoli.android.MainActivity;
 import io.ipoli.android.R;
 import io.ipoli.android.app.App;
+import io.ipoli.android.app.BaseFragment;
+import io.ipoli.android.app.help.HelpDialog;
+import io.ipoli.android.app.ui.EmptyStateRecyclerView;
+import io.ipoli.android.challenge.activities.ChallengeActivity;
 import io.ipoli.android.challenge.adapters.ChallengesAdapter;
 
 /**
  * Created by Venelin Valkov <venelin@curiousily.com>
  * on 5/27/16.
  */
-public class ChallengeListFragment extends Fragment {
+public class ChallengeListFragment extends BaseFragment {
 
     private Unbinder unbinder;
 
@@ -35,7 +41,13 @@ public class ChallengeListFragment extends Fragment {
     Bus eventBus;
 
     @BindView(R.id.challenge_list)
-    RecyclerView challengeList;
+    EmptyStateRecyclerView challengeList;
+
+    @BindView(R.id.root_container)
+    CoordinatorLayout rootLayout;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Nullable
     @Override
@@ -43,15 +55,27 @@ public class ChallengeListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_challenge_list, container, false);
         unbinder = ButterKnife.bind(this, view);
         App.getAppComponent(getContext()).inject(this);
+        ((MainActivity) getActivity()).initToolbar(toolbar, R.string.title_fragment_challenges);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         challengeList.setLayoutManager(layoutManager);
+        challengeList.setEmptyView(rootLayout, R.string.empty_text_chanllenge, R.drawable.ic_sword_grey_24dp);
 
         ChallengesAdapter adapter = new ChallengesAdapter(new ArrayList<>());
         challengeList.setAdapter(adapter);
 
         return view;
+    }
+
+    @Override
+    protected boolean useOptionsMenu() {
+        return true;
+    }
+
+    @Override
+    protected void showHelpDialog() {
+        HelpDialog.newInstance(R.layout.fragment_help_dialog_rewards, R.string.help_dialog_rewards_title, "rewards").show(getActivity().getSupportFragmentManager());
     }
 
     @Override
@@ -74,5 +98,6 @@ public class ChallengeListFragment extends Fragment {
 
     @OnClick(R.id.add_challenge)
     public void onAddReward(View view) {
+        startActivity(new Intent(getContext(), ChallengeActivity.class));
     }
 }
