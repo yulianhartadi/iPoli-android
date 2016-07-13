e083843736d5bd70e336ec9284b54f22953e6fc1
Polina Zhelyazkova <poly_vjk@abv.bg>
2016-07-13 11:53:03 +0300

    Cleanup

--- app/src/main/java/io/ipoli/android/quest/events/subquests/AddSubQuestTappedEvents.java
+++ app/src/main/java/io/ipoli/android/quest/events/subquests/AddSubQuestTappedEvents.java
@@ -6,10 +6,10 @@ import io.ipoli.android.app.events.EventSource;
  * Created by Polina Zhelyazkova <polina@ipoli.io>
  * on 7/11/16.
  */
-public class AddSubQuestTappedEvent {
+public class AddSubQuestTappedEvents {
     public final EventSource source;
 
-    public AddSubQuestTappedEvent(EventSource source) {
+    public AddSubQuestTappedEvents(EventSource source) {
         this.source = source;
     }
 }
