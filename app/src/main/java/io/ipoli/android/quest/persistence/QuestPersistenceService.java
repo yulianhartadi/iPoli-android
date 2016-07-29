db56571677372c9e82b047adf1811d4547992daf
Polina Zhelyazkova <poly_vjk@abv.bg>
2016-07-29 12:47:23 +0300

    Refactor

--- app/src/main/java/io/ipoli/android/quest/persistence/QuestPersistenceService.java
+++ app/src/main/java/io/ipoli/android/quest/persistence/QuestPersistenceService.java
@@ -72,7 +72,7 @@ public interface QuestPersistenceService extends PersistenceService<Quest> {
 
     void findAllCompleted(Challenge challenge, OnDataChangedListener<List<Quest>> listener);
 
-    long countCompleted(Challenge challenge, LocalDate start, LocalDate end);
+    void countCompletedByWeek(Challenge challenge, LocalDate start, LocalDate end, OnDataChangedListener<List<Long>> listener);
 
     void countCompleted(Challenge challenge, OnDataChangedListener<Long> listener);
 
