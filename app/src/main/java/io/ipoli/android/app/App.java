28a7386bdff0a66915bc8fa66475ca5eaee788b0
Polina Zhelyazkova <poly_vjk@abv.bg>
2016-08-09 16:40:18 +0300

    Revert to startAt and endAt queries. Fix setter in SourceMapping

--- app/src/main/java/io/ipoli/android/app/App.java
+++ app/src/main/java/io/ipoli/android/app/App.java
@@ -16,6 +16,7 @@ import android.text.TextUtils;
 import com.facebook.FacebookSdk;
 import com.flurry.android.FlurryAgent;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -188,7 +189,7 @@ public class App extends MultiDexApplication {
         JodaTimeAndroid.init(this);
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         getAppComponent(this).inject(this);
         registerServices();
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
--- app/src/main/java/io/ipoli/android/app/persistence/BaseFirebasePersistenceService.java
+++ app/src/main/java/io/ipoli/android/app/persistence/BaseFirebasePersistenceService.java
@@ -153,20 +153,14 @@ public abstract class BaseFirebasePersistenceService<T extends PersistedObject> 
 
     @Override
     public void listenForChange(OnChangeListener<List<T>> listener) {
-//        Query query = getCollectionReference().orderByChild("updatedAt").startAt(new Date().getTime());
-        Query query = getCollectionReference().orderByChild("updatedAt");
-        long now = new Date().getTime();
+        Query query = getCollectionReference().orderByChild("updatedAt").startAt(new Date().getTime());
         ChildEventListener childListener = new ChildEventListener() {
 
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String previousName) {
-                T child = dataSnapshot.getValue(getModelClass());
-                if(child.getUpdatedAt() >= now) {
-                    List<T> result = new ArrayList<>();
-//                    result.add(dataSnapshot.getValue(getModelClass()));
-                    result.add(child);
-                    listener.onNew(result);
-                }
+                List<T> result = new ArrayList<>();
+                result.add(dataSnapshot.getValue(getModelClass()));
+                listener.onNew(result);
             }
 
             @Override
