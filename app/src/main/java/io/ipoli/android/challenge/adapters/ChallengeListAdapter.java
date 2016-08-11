cfe5ba85c585f3131a2cbfb6854c52d794bf3183
Polina Zhelyazkova <poly_vjk@abv.bg>
2016-08-11 17:29:05 +0300

    Replace challenge list swipe layout with popup menu

--- app/src/main/java/io/ipoli/android/challenge/adapters/ChallengeListAdapter.java
+++ app/src/main/java/io/ipoli/android/challenge/adapters/ChallengeListAdapter.java
@@ -3,6 +3,7 @@ package io.ipoli.android.challenge.adapters;
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,6 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.chauthai.swipereveallayout.SwipeRevealLayout;
-import com.chauthai.swipereveallayout.ViewBinderHelper;
 import com.squareup.otto.Bus;
 
 import java.util.List;
@@ -40,13 +39,10 @@ public class ChallengeListAdapter extends RecyclerView.Adapter<ChallengeListAdap
     private final List<Challenge> challenges;
     private final Bus eventBus;
 
-    private final ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
-
     public ChallengeListAdapter(Context context, List<Challenge> challenges, Bus eventBus) {
         this.context = context;
         this.challenges = challenges;
         this.eventBus = eventBus;
-        viewBinderHelper.setOpenOnlyOne(true);
     }
 
     @Override
@@ -57,7 +53,6 @@ public class ChallengeListAdapter extends RecyclerView.Adapter<ChallengeListAdap
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         final Challenge challenge = challenges.get(position);
-        viewBinderHelper.bind(holder.swipeLayout, challenge.getId());
 
         Category category = Challenge.getCategory(challenge);
         GradientDrawable drawable = (GradientDrawable) holder.contextIndicatorBackground.getBackground();
@@ -70,22 +65,27 @@ public class ChallengeListAdapter extends RecyclerView.Adapter<ChallengeListAdap
 
         holder.name.setText(challenge.getName());
         holder.endDate.setText(DateFormatter.format(challenge.getEndDate()));
-        holder.swipeLayout.setSwipeListener(new SwipeRevealLayout.SimpleSwipeListener() {
-            @Override
-            public void onOpened(SwipeRevealLayout view) {
-                super.onOpened(view);
-                eventBus.post(new ItemActionsShownEvent(EventSource.CHALLENGES));
-            }
+
+        holder.moreMenu.setOnClickListener(v -> {
+            eventBus.post(new ItemActionsShownEvent(EventSource.CHALLENGES));
+            PopupMenu popupMenu = new PopupMenu(context, v);
+            popupMenu.inflate(R.menu.challenge_actions_menu);
+            popupMenu.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.complete_challenge:
+                        eventBus.post(new CompleteChallengeRequestEvent(challenge, EventSource.CHALLENGES));
+                        return true;
+                    case R.id.edit_challenge:
+                        eventBus.post(new EditChallengeRequestEvent(challenge, EventSource.CHALLENGES));
+                        return true;
+                    case R.id.delete_challenge:
+                        eventBus.post(new DeleteChallengeRequestEvent(challenge, EventSource.CHALLENGES));
+                        return true;
+                }
+                return false;
+            });
+            popupMenu.show();
         });
-
-        holder.completeChallenge.setOnClickListener(v -> eventBus.post(new CompleteChallengeRequestEvent(challenge, EventSource.CHALLENGES)));
-
-        holder.editChallenge.setOnClickListener(v -> {
-            holder.swipeLayout.close(true);
-            eventBus.post(new EditChallengeRequestEvent(challenge, EventSource.CHALLENGES));
-        });
-
-        holder.deleteChallenge.setOnClickListener(v -> eventBus.post(new DeleteChallengeRequestEvent(challenge, EventSource.CHALLENGES)));
     }
 
     @Override
@@ -110,17 +110,8 @@ public class ChallengeListAdapter extends RecyclerView.Adapter<ChallengeListAdap
         @BindView(R.id.challenge_category_indicator_image)
         ImageView contextIndicatorImage;
 
-        @BindView(R.id.swipe_layout)
-        SwipeRevealLayout swipeLayout;
-
-        @BindView(R.id.complete_challenge)
-        ImageButton completeChallenge;
-
-        @BindView(R.id.edit_challenge)
-        ImageButton editChallenge;
-
-        @BindView(R.id.delete_challenge)
-        ImageButton deleteChallenge;
+        @BindView(R.id.challenge_more_menu)
+        ImageButton moreMenu;
 
         public ViewHolder(View itemView) {
             super(itemView);
