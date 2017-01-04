7409e4bb8a18ccf77a9e4b04a1b7895b4b703c0a
Venelin Valkov <venelin@curiousily.com>
2017-01-04 10:45:57 +0200

    Show day quests in agenda calendar

--- app/src/main/java/io/ipoli/android/quest/activities/AgendaActivity.java
+++ app/src/main/java/io/ipoli/android/quest/activities/AgendaActivity.java
@@ -2,32 +2,29 @@ package io.ipoli.android.quest.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
 
 import org.joda.time.LocalDate;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.ipoli.android.R;
 import io.ipoli.android.app.activities.BaseActivity;
-import io.ipoli.android.app.utils.Time;
 import io.ipoli.android.quest.adapters.AgendaAdapter;
-import io.ipoli.android.quest.data.Category;
-import io.ipoli.android.quest.data.Quest;
+import io.ipoli.android.quest.persistence.QuestPersistenceService;
 
 /**
  * Created by Venelin Valkov <venelin@curiousily.com>
@@ -35,6 +32,9 @@ import io.ipoli.android.quest.data.Quest;
  */
 
 public class AgendaActivity extends BaseActivity implements CompactCalendarView.CompactCalendarViewListener {
+
+    @Inject
+    QuestPersistenceService questPersistenceService;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -44,6 +44,9 @@ public class AgendaActivity extends BaseActivity implements CompactCalendarView.
 
     @BindView(R.id.agenda_list)
     RecyclerView questList;
+
+    @BindView(R.id.agenda_journey_text)
+    TextView journeyText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -60,50 +63,41 @@ public class AgendaActivity extends BaseActivity implements CompactCalendarView.
             ab.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
         }
 
-        LocalDate date = LocalDate.now();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(getString(getToolbarText(date)), Locale.getDefault());
-        getSupportActionBar().setTitle(simpleDateFormat.format(date.toDate()));
-
-        Calendar tDate = Calendar.getInstance();
-        tDate.add(Calendar.DAY_OF_YEAR, -3);
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_light_green_500), tDate.getTimeInMillis()), true);
-
-        Calendar yesterday = Calendar.getInstance();
-        yesterday.add(Calendar.DAY_OF_YEAR, -1);
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_orange_500), yesterday.getTimeInMillis()), true);
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_orange_500), yesterday.getTimeInMillis()), true);
-
-        Calendar tomorrow = Calendar.getInstance();
-        tomorrow.add(Calendar.DAY_OF_YEAR, 1);
-
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_red_500), tomorrow.getTimeInMillis()), true);
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_red_500), tomorrow.getTimeInMillis()), true);
-        calendar.addEvent(new Event(ContextCompat.getColor(this, R.color.md_red_500), tomorrow.getTimeInMillis()), true);
-
         calendar.setListener(this);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         questList.setLayoutManager(layoutManager);
         questList.setHasFixedSize(true);
 
+        showQuestsForDate(new Date());
+    }
 
-        List<Quest> quests = new ArrayList<>();
-        Quest q1 = new Quest("Clean up the fridge");
-        q1.setStartTime(Time.at(9, 0));
-        q1.setDuration(30);
-        q1.setCategoryType(Category.CHORES);
-        quests.add(q1);
-        Quest q2 = new Quest("Study Deep Learning book");
-        q2.setStartTime(Time.at(11, 30));
-        q2.setDuration(120);
-        q2.setCategoryType(Category.LEARNING);
-        quests.add(q2);
-        Quest q3 = new Quest("Hit the gym");
-        q3.setCategoryType(Category.WELLNESS);
-        q3.setStartTime(Time.at(20, 0));
-        q3.setDuration(90);
-        quests.add(q3);
-        questList.setAdapter(new AgendaAdapter(quests));
+    private String getDayNumberSuffix(int day) {
+        if (day >= 11 && day <= 13) {
+            return "th";
+        }
+        switch (day % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+
+    private void showQuestsForDate(Date newDate) {
+        LocalDate date = new LocalDate(newDate);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(getString(getToolbarText(date)), Locale.getDefault());
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(simpleDateFormat.format(date.toDate()));
+        }
+        String dayNumberSuffix = getDayNumberSuffix(date.getDayOfMonth());
+        DateFormat dateFormat = new SimpleDateFormat(getString(R.string.agenda_daily_journey_format, dayNumberSuffix));
+        journeyText.setText(getString(R.string.agenda_daily_journey, dateFormat.format(newDate)));
+        questPersistenceService.findAllNonAllDayForDate(date, quests -> questList.setAdapter(new AgendaAdapter(quests)));
     }
 
     private int getToolbarText(LocalDate date) {
@@ -116,20 +110,16 @@ public class AgendaActivity extends BaseActivity implements CompactCalendarView.
         if (date.isEqual(new LocalDate().plusDays(1))) {
             return R.string.tomorrow_calendar_format;
         }
-        return R.string.calendar_format;
+        return R.string.agenda_calendar_format;
     }
 
     @Override
     public void onDayClick(Date dateClicked) {
-        LocalDate date = new LocalDate(dateClicked);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(getString(getToolbarText(date)), Locale.getDefault());
-        getSupportActionBar().setTitle(simpleDateFormat.format(date.toDate()));
+        showQuestsForDate(dateClicked);
     }
 
     @Override
     public void onMonthScroll(Date firstDayOfNewMonth) {
-        LocalDate date = new LocalDate(firstDayOfNewMonth);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(getString(getToolbarText(date)), Locale.getDefault());
-        getSupportActionBar().setTitle(simpleDateFormat.format(date.toDate()));
+        showQuestsForDate(firstDayOfNewMonth);
     }
 }
