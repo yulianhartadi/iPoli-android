fa9238b0c25007acea5f79d53ed6d12389a03dce
Venelin Valkov <venelin@curiousily.com>
2017-01-04 09:32:44 +0200

    Cleanup

--- app/src/main/java/io/ipoli/android/MainActivity.java
+++ app/src/main/java/io/ipoli/android/MainActivity.java
@@ -172,10 +172,6 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         startCalendar();
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
             @Override
-            public void onDrawerOpened(View drawerView) {
-            }
-
-            @Override
             public void onDrawerClosed(View drawerView) {
                 onNavigationDrawerClosed();
             }
