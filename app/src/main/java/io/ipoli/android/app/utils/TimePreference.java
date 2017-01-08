623715ec20ae3af9bad9f8b254658d9e0b9bb155
Venelin Valkov <venelin@curiousily.com>
2017-01-08 20:14:52 +0200

    Choose time for quest

---
+++ app/src/main/java/io/ipoli/android/app/utils/TimePreference.java
@@ -0,0 +1,9 @@
+package io.ipoli.android.app.utils;
+
+/**
+ * Created by Venelin Valkov <venelin@curiousily.com>
+ * on 1/8/17.
+ */
+public enum TimePreference {
+    WORK_HOURS, PERSONAL_HOURS, MORNING, AFTERNOON, EVENING, ANY
+}
