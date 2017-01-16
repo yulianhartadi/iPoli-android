763f4f8f474bf53be77afc8e10a736a96b336472
Venelin Valkov <venelin@curiousily.com>
2017-01-16 18:57:26 +0200

    Update migration

--- app/src/main/java/io/ipoli/android/app/activities/MigrationActivity.java
+++ app/src/main/java/io/ipoli/android/app/activities/MigrationActivity.java
@@ -4,19 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.joda.time.LocalDate;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import butterknife.ButterKnife;
@@ -24,10 +17,7 @@ import io.ipoli.android.Constants;
 import io.ipoli.android.MainActivity;
 import io.ipoli.android.R;
 import io.ipoli.android.app.App;
-import io.ipoli.android.app.events.AppErrorEvent;
 import io.ipoli.android.app.events.InitAppEvent;
-import io.ipoli.android.app.exceptions.MigrationException;
-import io.ipoli.android.app.utils.DateUtils;
 
 /**
  * Created by Venelin Valkov <venelin@curiousily.com>
@@ -89,19 +79,39 @@ public class MigrationActivity extends BaseActivity {
                     }
                 }
 
-//                if (data.containsKey("challenges")) {
-//
-//                }
+                if (data.containsKey("challenges")) {
+                    Map<String, Object> challenges = (Map<String, Object>) data.get("challenges");
 
-                data.put("/schemaVersion", Constants.SCHEMA_VERSION);
+                    for (Object challenge : challenges.values()) {
+                        Map<String, Object> challengeData = (Map<String, Object>) challenge;
+
+                        if (challengeData.containsKey("challengeQuests")) {
+                            Map<String, Object> quests = (Map<String, Object>) data.get("challengeQuests");
+                            for (Object q : quests.values()) {
+                                updateQuestData((Map<String, Object>) q);
+                            }
+                        }
+
+                        if (challengeData.containsKey("challengeRepeatingQuests")) {
+                            Map<String, Object> quests = (Map<String, Object>) data.get("challengeRepeatingQuests");
+                            for (Object q : quests.values()) {
+                                updateQuestData((Map<String, Object>) q);
+                            }
+                        }
+
+                    }
+                }
+
+                data.put("schemaVersion", Constants.SCHEMA_VERSION);
 
                 db.getReference("/v1/players/" + playerId).updateChildren(data, (error, dbRef) -> {
 
                     if (error != null) {
-                        eventBus.post(new AppErrorEvent(new MigrationException("Unable to update player children: " + playerId, error.toException())));
-                        Toast.makeText(MigrationActivity.this, R.string.migration_error_message, Toast.LENGTH_LONG).show();
-                        finish();
-                        return;
+                        throw new RuntimeException(error.toException());
+//                        eventBus.post(new AppErrorEvent(new MigrationException("Unable to update player children: " + playerId, error.toException())));
+//                        Toast.makeText(MigrationActivity.this, R.string.migration_error_message, Toast.LENGTH_LONG).show();
+//                        finish();
+//                        return;
                     }
                     localStorage.saveInt(Constants.KEY_SCHEMA_VERSION, Constants.SCHEMA_VERSION);
                     eventBus.post(new InitAppEvent());
@@ -112,9 +122,10 @@ public class MigrationActivity extends BaseActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                eventBus.post(new AppErrorEvent(new MigrationException("Unable to find player: " + playerId, databaseError.toException())));
-                Toast.makeText(MigrationActivity.this, R.string.migration_error_message, Toast.LENGTH_LONG).show();
-                finish();
+                throw new RuntimeException(databaseError.toException());
+//                eventBus.post(new AppErrorEvent(new MigrationException("Unable to find player: " + playerId, databaseError.toException())));
+//                Toast.makeText(MigrationActivity.this, R.string.migration_error_message, Toast.LENGTH_LONG).show();
+//                finish();
             }
         });
     }
@@ -130,150 +141,6 @@ public class MigrationActivity extends BaseActivity {
             questData.put("scheduled", questData.get("end"));
         }
         questData.put("preferredStartTime", "ANY");
-    }
-
-    private void populateRepeatingQuest(Map<String, Object> repeatingQuest, List<Map<String, Object>> quests, Map<String, Object> data) {
-        List<Map<String, Object>> questsToSave = new ArrayList<>();
-        Long timesADay = (Long) repeatingQuest.get("timesADay");
-        String rqId = repeatingQuest.get("id").toString();
-
-        if (repeatingQuest.containsKey("challengeId")) {
-            String challengeId = repeatingQuest.get("challengeId").toString();
-            data.put("/challenges/" + challengeId + "/repeatingQuestIds/" + rqId, true);
-            data.put("/challenges/" + challengeId + "/challengeRepeatingQuests/" + rqId, repeatingQuest);
-        }
-
-        if (quests == null || quests.isEmpty()) {
-            return;
-        }
-
-        if (timesADay == 1) {
-
-            for (Map<String, Object> quest : quests) {
-                quest = copyQuest(quest);
-                quest.put("repeatingQuestId", rqId);
-                quest.remove("repeatingQuest");
-                questsToSave.add(quest);
-            }
-        } else {
-            Map<Long, List<Map<String, Object>>> dateToQuests = new HashMap<>();
-            for (Map<String, Object> quest : quests) {
-                quest = copyQuest(quest);
-                quest.put("timesADay", timesADay);
-                quest.put("repeatingQuestId", rqId);
-                quest.remove("repeatingQuest");
-
-                if (!dateToQuests.containsKey(quest.get("end"))) {
-                    dateToQuests.put((Long) quest.get("end"), new ArrayList<>());
-                }
-                dateToQuests.get(quest.get("end")).add(quest);
-            }
-
-            for (List<Map<String, Object>> dateQuests : dateToQuests.values()) {
-                int completedCount = 0;
-                for (Map<String, Object> quest : dateQuests) {
-                    if (quest.containsKey("completedAt")) {
-                        completedCount++;
-                    }
-                }
-                Map<String, Object> questToSave = dateQuests.get(0);
-                questToSave.put("completedCount", completedCount);
-                if (completedCount != timesADay) {
-                    questToSave.remove("completedAt");
-                    questToSave.remove("completedAtMinute");
-                }
-                questsToSave.add(questToSave);
-            }
-        }
-
-        for (Map<String, Object> quest : questsToSave) {
-            populateQuest(quest, data);
-            data.put("/quests/" + quest.get("id"), quest);
-            ((Map<String, Object>) repeatingQuest.get("questsData")).put(quest.get("id").toString(), createQuestData(quest));
-        }
-    }
-
-    private Map<String, Object> copyRepeatingQuest(Map<String, Object> rq) {
-        rq.put("timesADay", ((Map<String, Object>) rq.get("recurrence")).get("timesADay"));
-        ((Map<String, Object>) rq.get("recurrence")).remove("timesADay");
-        rq.put("questsData", new HashMap<>());
-        return rq;
-    }
-
-    private Map<String, Object> copyQuest(Map<String, Object> quest) {
-        int completedCount = 0;
-        if (quest.containsKey("completedAt")) {
-            Date newEnd = DateUtils.toStartOfDayUTC(new LocalDate(Long.valueOf(quest.get("completedAt").toString())));
-            quest.put("end", newEnd.getTime());
-            completedCount = 1;
-        }
-
-        quest.put("timesADay", 1);
-        quest.put("completedCount", completedCount);
-        quest.put("reminderStartTimes", new ArrayList<Long>());
-        return quest;
-    }
-
-    private void populateQuest(Map<String, Object> quest, Map<String, Object> data) {
-        String questId = (String) quest.get("id");
-        if (quest.containsKey("completedAt")) {
-            data.put("/dayQuests/" + quest.get("end") + "/" + questId, quest);
-        } else if (!quest.containsKey("end")) {
-            data.put("/inboxQuests/" + questId, quest);
-        } else {
-            data.put("/dayQuests/" + quest.get("end") + "/" + questId, quest);
-
-            if (shouldAddQuestReminders(quest)) {
-                createQuestReminders(quest, data);
-            }
-        }
-        if (quest.containsKey("challengeId")) {
-            String challengeId = quest.get("challengeId").toString();
-            data.put("/challenges/" + challengeId + "/questsData/" + questId, createQuestData(quest));
-            if (!quest.containsKey("repeatingQuest")) {
-                data.put("/challenges/" + challengeId + "/challengeQuests/" + questId, quest);
-            }
-        }
-    }
-
-    private Map<String, Object> createQuestData(Map<String, Object> quest) {
-        Long duration = (Long) quest.get("duration");
-        if (quest.containsKey("completedAt") && quest.containsKey("actualStart")) {
-            duration = ((((Long) quest.get("completedAt")) - ((Long) quest.get("actualStart"))) / 60000);
-        }
-        Long finalDuration = duration;
-        return new HashMap<String, Object>() {{
-            put("complete", quest.containsKey("completedAt"));
-            put("duration", finalDuration);
-            put("originalScheduledDate", quest.containsKey("originalStart") ? quest.get("originalStart") : null);
-            put("scheduledDate", quest.containsKey("end") ? quest.get("end") : null);
-        }};
-    }
-
-    private void createQuestReminders(Map<String, Object> quest, Map<String, Object> data) {
-        for (Map<String, Object> reminder : (List<Map<String, Object>>) quest.get("reminders")) {
-            List<Long> reminderStartTimes = (List<Long>) quest.get("reminderStartTimes");
-            Long reminderStart = (Long) reminder.get("start");
-            reminderStartTimes.add(reminderStart);
-            data.put("/questReminders/" + reminderStart + "/" + quest.get("id").toString(), createQuestReminder(quest, reminder));
-        }
-    }
-
-    private Map<String, Object> createQuestReminder(Map<String, Object> quest, Map<String, Object> reminder) {
-        return new HashMap<String, Object>() {{
-            put("minutesFromStart", reminder.get("minutesFromStart"));
-            put("notificationId", reminder.get("notificationId"));
-            put("questId", quest.get("id"));
-            put("questName", quest.get("name"));
-            put("start", reminder.get("start"));
-
-        }};
-    }
-
-    private boolean shouldAddQuestReminders(Map<String, Object> quest) {
-        return !quest.containsKey("completedAt") && quest.containsKey("end") && quest.containsKey("startMinute") &&
-                (((Long) quest.get("startMinute")) >= 0) && quest.containsKey("reminders") && ((List<Object>) quest.get("reminders")).size() > 0 &&
-                !quest.containsKey("actualStart");
     }
 
     @Override
