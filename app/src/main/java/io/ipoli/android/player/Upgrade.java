745698649f42f145b3ef5458fdad3a70e15a1425
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-05-24 17:26:57 +0300

    Add properties to upgrade enum

--- app/src/main/java/io/ipoli/android/player/Upgrade.java
+++ app/src/main/java/io/ipoli/android/player/Upgrade.java
@@ -1,26 +1,55 @@
 package io.ipoli.android.player;
 
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+
+import io.ipoli.android.R;
+
 /**
  * Created by Polina Zhelyazkova <polina@ipoli.io>
  * on 5/21/17.
  */
 
 public enum Upgrade {
-    CHALLENGES(1, 300),
-    NOTES(2, 200),
-    REMINDERS(3, 100),
-    CALENDAR_SYNC(4, 500),
-    SUB_QUESTS(5, 300),
-    REPEATING_QUESTS(6, 20),
-    TIMER(7, 200),
-    CUSTOM_DURATION(8, 200);
+    CHALLENGES(1, 300, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_sword_white_24dp),
+    NOTES(2, 200, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_note_white_24dp),
+    REMINDERS(3, 100, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_reminders_white_24dp),
+    CALENDAR_SYNC(4, 500, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_calendar_blank_grey_24dp),
+    SUB_QUESTS(5, 300, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_format_list_bulleted_white_24dp),
+    REPEATING_QUESTS(6, 20, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_repeat_white_24dp),
+    TIMER(7, 200, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_timer_white_24dp),
+    CUSTOM_DURATION(8, 200, R.string.help_dialog_challenge_title,
+            R.string.challenge1_desc, R.string.challenge2_desc, R.drawable.ic_today_white_24dp);
 
     private final int code;
     private final int price;
+    @StringRes
+    private final int title;
 
-    Upgrade(int code, int price) {
+    @StringRes
+    private final int shortDesc;
+
+    @StringRes
+    private final int longDesc;
+
+    @DrawableRes
+    private final int image;
+
+    Upgrade(int code, int price, @StringRes int title, @StringRes int shortDesc,
+            @StringRes int longDesc, @DrawableRes int image) {
         this.code = code;
         this.price = price;
+        this.title = title;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+        this.image = image;
     }
 
     public int getCode() {
@@ -31,9 +60,25 @@ public enum Upgrade {
         return price;
     }
 
+    public int getTitle() {
+        return title;
+    }
+
+    public int getShortDesc() {
+        return shortDesc;
+    }
+
+    public int getLongDesc() {
+        return longDesc;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
     public static Upgrade get(int code) {
-        for(Upgrade upgrade : values()) {
-            if(upgrade.code == code) {
+        for (Upgrade upgrade : values()) {
+            if (upgrade.code == code) {
                 return upgrade;
             }
         }
