12bc0754ae21e9b28ada31934fc6049e3eb1038a
Venelin Valkov <venelin@curiousily.com>
2017-05-27 01:51:18 +0300

    Init toolbar

--- app/src/main/java/io/ipoli/android/quest/activities/EisenhowerMatrixActivity.java
+++ app/src/main/java/io/ipoli/android/quest/activities/EisenhowerMatrixActivity.java
@@ -2,7 +2,10 @@ package io.ipoli.android.quest.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.ipoli.android.R;
 import io.ipoli.android.app.activities.BaseActivity;
@@ -13,6 +16,9 @@ import io.ipoli.android.app.activities.BaseActivity;
  */
 public class EisenhowerMatrixActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         appComponent().inject(this);
@@ -20,5 +26,11 @@ public class EisenhowerMatrixActivity extends BaseActivity {
         setContentView(R.layout.activity_eisenhower_matrix);
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+        }
     }
 }
