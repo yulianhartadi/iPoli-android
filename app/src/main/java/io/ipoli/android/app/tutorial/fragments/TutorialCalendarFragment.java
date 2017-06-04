0af3f433f04a21ef9a96dfe16957bf61b87e18c0
Venelin Valkov <venelin@curiousily.com>
2017-06-04 17:58:58 +0300

    Add first quest tutorial

---
+++ app/src/main/java/io/ipoli/android/app/tutorial/fragments/TutorialCalendarFragment.java
@@ -0,0 +1,10 @@
+package io.ipoli.android.app.tutorial.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Venelin Valkov <venelin@curiousily.com>
+ * on 6/4/17.
+ */
+public class TutorialCalendarFragment extends Fragment {
+}
