b06dec544859bf83dafca79b6dd8f480f1f45d02
Venelin Valkov <venelin@curiousily.com>
2017-06-05 15:55:15 +0300

    Fix animation sliding direction

--- app/src/main/java/io/ipoli/android/app/tutorial/OnboardingActivity.java
+++ app/src/main/java/io/ipoli/android/app/tutorial/OnboardingActivity.java
@@ -2,9 +2,9 @@ package io.ipoli.android.app.tutorial;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import io.ipoli.android.R;
 import io.ipoli.android.app.tutorial.fragments.TutorialAddQuestFragment;
@@ -26,60 +26,53 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction =
-                fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.root_container, new TutorialIntroFragment());
-//        fragmentTransaction.replace(R.id.root_container, new TutorialAddQuestFragment());
-//        fragmentTransaction.replace(R.id.root_container, new TutorialOutroFragment());
-        fragmentTransaction.commit();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.root_container, new TutorialIntroFragment())
+                .commit();
     }
 
-//    @Override
-//    public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus) {
-//            getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//        }
-//    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 
     public void onIntroDone() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-        transaction.replace(R.id.root_container, new TutorialNamePromptFragment(), "fragment");
-        transaction.commit();
+        replaceFragment(new TutorialNamePromptFragment());
     }
 
     public void onNamePromptDone(String playerName) {
         this.playerName = playerName;
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-        transaction.replace(R.id.root_container, new TutorialAddQuestFragment(), "fragment");
-        transaction.commit();
+        replaceFragment(new TutorialAddQuestFragment());
     }
 
     public void onAddQuestDone(String name, Category category) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         TutorialCalendarFragment calendarFragment = new TutorialCalendarFragment();
         calendarFragment.setQuestInfo(name, category);
-        transaction.replace(R.id.root_container, calendarFragment, "fragment");
-        transaction.commit();
+        replaceFragment(calendarFragment);
     }
 
     public void onCalendarDone() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         TutorialOutroFragment outroFragment = new TutorialOutroFragment();
         outroFragment.setPlayerName(playerName);
-        transaction.replace(R.id.root_container, outroFragment, "fragment");
-        transaction.commit();
+        replaceFragment(outroFragment);
+    }
+
+    private void replaceFragment(Fragment newFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)
+                .replace(R.id.root_container, newFragment, "fragment")
+                .commit();
     }
 
     public void onTutorialDone() {
