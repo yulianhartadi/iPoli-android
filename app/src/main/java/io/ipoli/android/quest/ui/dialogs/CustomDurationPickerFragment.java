545a54922bf5dbf8cf581a62350ae6e3862da891
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-06-07 23:54:59 +0300

    Duration picker layout

--- app/src/main/java/io/ipoli/android/quest/ui/dialogs/CustomDurationPickerFragment.java
+++ app/src/main/java/io/ipoli/android/quest/ui/dialogs/CustomDurationPickerFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -58,12 +59,22 @@ public class CustomDurationPickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.fragment_text_picker, null);
+        View titleView = inflater.inflate(R.layout.custom_duration_dialog_header, null);
+
+        TextView hours = (TextView) titleView.findViewById(R.id.custom_duration_hours);
+        TextView minutes = (TextView) titleView.findViewById(R.id.custom_duration_minutes);
+
+        hours.setText("00");
+        minutes.setText("45");
+
+
+
+        View view = inflater.inflate(R.layout.fragment_custom_duration_picker, null);
         unbinder = ButterKnife.bind(this, view);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setIcon(R.drawable.logo)
-                .setTitle(R.string.quest_duration_question)
+                .setCustomTitle(titleView)
                 .setView(view)
                 .setPositiveButton(getString(R.string.help_dialog_ok), (dialog, which) -> {
 
