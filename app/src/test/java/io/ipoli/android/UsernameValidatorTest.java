07865999c652a1f66a9d264a926f5de947cd947d
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-07-01 18:17:18 +0300

    Use annotations in UsernameValidatorTest

--- app/src/test/java/io/ipoli/android/UsernameValidatorTest.java
+++ app/src/test/java/io/ipoli/android/UsernameValidatorTest.java
@@ -2,7 +2,10 @@ package io.ipoli.android;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
 import io.ipoli.android.app.persistence.OnDataChangedListener;
@@ -12,27 +15,26 @@ import io.ipoli.android.feed.persistence.FeedPersistenceService;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 /**
  * Created by Venelin Valkov <venelin@curiousily.com>
  * on 7/1/17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class UsernameValidatorTest {
 
+    @Mock
     private FeedPersistenceService defaultFeedPersistenceService;
 
+    @Mock
     private FeedPersistenceService notUniqueFeedPersistenceService;
 
+    @Mock
     private UsernameValidator.ResultListener resultListener;
 
     @Before
     public void setUp() {
-        resultListener = mock(UsernameValidator.ResultListener.class);
-        defaultFeedPersistenceService = mock(FeedPersistenceService.class);
-        notUniqueFeedPersistenceService = mock(FeedPersistenceService.class);
-
         doAnswer(new Answer<Void>() {
             @Override
             public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
@@ -57,6 +59,12 @@ public class UsernameValidatorTest {
     public void validate_validUsername_callOnValid() {
         UsernameValidator.validate("iPoli", defaultFeedPersistenceService, resultListener);
         verify(resultListener).onValid();
+    }
+
+    @Test
+    public void validate_nullUsername_callOnInvalidWithEmptyError() {
+        UsernameValidator.validate(null, defaultFeedPersistenceService, resultListener);
+        verify(resultListener).onInvalid(UsernameValidator.UsernameValidationError.EMPTY);
     }
 
     @Test
