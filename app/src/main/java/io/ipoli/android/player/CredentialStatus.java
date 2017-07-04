83806fa3c6da45ef7e1af75456bab628c0863289
Venelin Valkov <venelin@curiousily.com>
2017-07-04 19:14:07 +0300

    Extract CredentialStatus

---
+++ app/src/main/java/io/ipoli/android/player/CredentialStatus.java
@@ -0,0 +1,9 @@
+package io.ipoli.android.player;
+
+/**
+ * Created by Venelin Valkov <venelin@curiousily.com>
+ * on 7/4/17.
+ */
+public enum CredentialStatus {
+    AUTHORIZED, GUEST, NO_USERNAME
+}
