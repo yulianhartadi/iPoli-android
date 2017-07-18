f5002c608c6494e0a878fc13f0c3165d4b76f2ee
Venelin Valkov <venelin@curiousily.com>
2017-07-18 14:44:33 +0300

    Reduce PowerUp prices

--- app/src/main/java/io/ipoli/android/store/PowerUp.java
+++ app/src/main/java/io/ipoli/android/store/PowerUp.java
@@ -10,39 +10,40 @@ import io.ipoli.android.R;
  * on 5/21/17.
  */
 public enum PowerUp {
-    REMINDERS(1, 300, R.string.reminders, R.string.power_up_reminders_sub_title,
+
+    REMINDERS(1, 130, R.string.reminders, R.string.power_up_reminders_sub_title,
             R.string.power_up_reminders_short_desc, R.string.power_up_reminders_long_desc, R.drawable.ic_reminders_white_24dp),
-    
-    CHALLENGES(3, 500, R.string.challenges, R.string.power_up_challenges_sub_title,
+
+    CHALLENGES(3, 220, R.string.challenges, R.string.power_up_challenges_sub_title,
             R.string.power_up_challenges_short_desc, R.string.power_up_challenges_long_desc, R.drawable.ic_sword_white_24dp),
 
-    CALENDAR_SYNC(4, 1000, R.string.settings_sync_google_calendars, R.string.power_up_sync_calendars_sub_title,
+    CALENDAR_SYNC(4, 450, R.string.settings_sync_google_calendars, R.string.power_up_sync_calendars_sub_title,
             R.string.power_up_sync_calendars_short_desc, R.string.power_up_sync_calendars_long_desc, R.drawable.ic_event_white_24dp),
 
-    TIMER(5, 300, R.string.timer, R.string.power_up_timer_sub_title,
+    TIMER(5, 130, R.string.timer, R.string.power_up_timer_sub_title,
             R.string.power_up_timer_short_desc, R.string.power_up_timer_long_desc, R.drawable.ic_timer_white_24dp),
 
-    GROWTH(6, 500, R.string.growth, R.string.power_up_growth_sub_title,
+    GROWTH(6, 220, R.string.growth, R.string.power_up_growth_sub_title,
             R.string.power_up_growth_short_desc, R.string.power_up_growth_long_desc, R.drawable.ic_growth_white_24dp),
 
-    EISENHOWER_MATRIX(7, 500, R.string.title_activity_eisenhower_matrix, R.string.power_up_matrix_sub_title,
+    EISENHOWER_MATRIX(7, 220, R.string.title_activity_eisenhower_matrix, R.string.power_up_matrix_sub_title,
             R.string.power_up_matrix_short_desc, R.string.power_up_matrix_long_desc, R.drawable.ic_matrix_white_24dp),
 
-    PREDEFINED_CHALLENGES(8, 1000, R.string.programmed_challenges, R.string.power_up_predefined_challenges_sub_title,
+    PREDEFINED_CHALLENGES(8, 450, R.string.programmed_challenges, R.string.power_up_predefined_challenges_sub_title,
             R.string.power_up_predefined_challenges_short_desc, R.string.power_up_predefined_challenges_long_desc,
             R.drawable.ic_sword_white_24dp, CHALLENGES),
 
-    SUB_QUESTS(9, 400, R.string.sub_quests, R.string.power_up_sub_quests_sub_title,
+    SUB_QUESTS(9, 180, R.string.sub_quests, R.string.power_up_sub_quests_sub_title,
             R.string.power_up_sub_quests_short_desc, R.string.power_up_sub_quests_long_desc, R.drawable.ic_format_list_bulleted_white_24dp),
 
-    NOTES(10, 200, R.string.notes, R.string.power_up_notes_sub_title,
+    NOTES(10, 90, R.string.notes, R.string.power_up_notes_sub_title,
             R.string.power_up_notes_short_desc, R.string.power_up_notes_long_desc, R.drawable.ic_note_white_24dp),
 
-    CUSTOM_DURATION(11, 300, R.string.custom_duration, R.string.power_up_custom_duration_sub_title,
+    CUSTOM_DURATION(11, 130, R.string.custom_duration, R.string.power_up_custom_duration_sub_title,
             R.string.power_up_custom_duration_short_desc, R.string.power_up_custom_duration_long_desc, R.drawable.ic_timer_white_24dp);
 
-
     public final int code;
+
     public final int price;
 
     @StringRes
