91111ed319c184dbf158a2403d02dc25c6c815fc
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-07-24 19:57:29 +0300

    Add all actions

--- app/src/main/java/io/ipoli/android/achievement/AchievementAction.java
+++ app/src/main/java/io/ipoli/android/achievement/AchievementAction.java
@@ -6,6 +6,21 @@ package io.ipoli.android.achievement;
  */
 public enum AchievementAction {
     COMPLETE_QUEST,
+    SHARE_QUEST,
+    COMPLETE_REPEATING_QUEST,
+    CREATE_CHALLENGE,
     COMPLETE_DAILY_CHALLENGE,
-    COMPLETE_CHALLENGE
+    COMPLETE_CHALLENGE,
+    USE_REWARD,
+    CHANGE_AVATAR,
+    CHANGE_PET,
+    LEVEL_UP,
+    BUY_POWER_UP,
+    WIN_COINS,
+    WIN_XP,
+    INVITE_FRIEND,
+    PET_DIED,
+    FOLLOW,
+    IS_FOLLOWED,
+    SEND_FEEDBACK
 }
