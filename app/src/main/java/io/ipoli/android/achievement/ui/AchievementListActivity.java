8958d7a86ca7e3b9b62c4d5b9be7a95e34072f87
Venelin Valkov <venelin@curiousily.com>
2017-07-28 13:56:03 +0300

    Add Achievement list activity

--- app/src/main/java/io/ipoli/android/achievement/ui/AchievementListActivity.java
+++ app/src/main/java/io/ipoli/android/achievement/ui/AchievementListActivity.java
@@ -2,9 +2,15 @@ package io.ipoli.android.achievement.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.ipoli.android.R;
+import io.ipoli.android.achievement.ui.adapters.AchievementAdapter;
 import io.ipoli.android.app.App;
 import io.ipoli.android.app.activities.BaseActivity;
 
@@ -14,11 +20,25 @@ import io.ipoli.android.app.activities.BaseActivity;
  */
 public class AchievementListActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.achievement_list)
+    RecyclerView achievementList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         App.getAppComponent(this).inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement_list);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        achievementList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        achievementList.setAdapter(new AchievementAdapter());
     }
 }
