eba3cd101cdaba8ffc29461777e4532b9a91e3d4
Venelin Valkov <venelin@curiousily.com>
2017-08-18 11:17:00 +0300

    Rewrite test using test observable

--- app/src/test/java/io/ipoli/android/DisplayRewardUseCaseTest.kt
+++ app/src/test/java/io/ipoli/android/DisplayRewardUseCaseTest.kt
@@ -4,20 +4,18 @@ import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import io.ipoli.android.player.Player
 import io.ipoli.android.player.PlayerRepository
-import io.ipoli.android.reward.DisplayRewardsUseCase
-import io.ipoli.android.reward.Reward
-import io.ipoli.android.reward.RewardRepository
-import io.ipoli.android.reward.RewardsLoadedPartialChange
+import io.ipoli.android.reward.*
 import io.ipoli.android.util.RxSchedulersTestRule
 import io.reactivex.Observable
-import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert.assertThat
+import io.reactivex.observers.TestObserver
+import io.reactivex.schedulers.Schedulers
 import org.junit.Rule
 import org.junit.Test
 
 
 /**
- * Created by Venelin Valkov <venelin@curiousily.com> on 7/10/17.
+ * Created by Venelin Valkov <venelin@curiousily.com>
+ * on 7/10/17.
  */
 class DisplayRewardUseCaseTest {
 
@@ -36,10 +34,22 @@ class DisplayRewardUseCaseTest {
             on { loadRewards() } doReturn Observable.just(listOf(Reward()))
         }
 
-        val result = DisplayRewardsUseCase(rewardRepoMock, playerRepoMock).execute(Unit).blockingIterable()
-        assertThat(result.count(), `is`(2))
+        val useCase = DisplayRewardsUseCase(rewardRepoMock, playerRepoMock)
+        useCase.subscribeOnScheduler = Schedulers.io()
 
-        val loadedState = result.elementAt(1) as RewardsLoadedPartialChange
-        assertThat(loadedState.data.size, `is`(1))
+        val observer = TestObserver<RewardStatePartialChange>()
+
+        useCase.execute(Unit)
+                .subscribe(observer)
+
+        observer.assertComplete()
+        observer.assertNoErrors()
+        observer.assertValueCount(2)
+        observer.assertValueAt(0, { it is RewardsLoadingPartialChange })
+        observer.assertValueAt(1, { it is RewardsLoadedPartialChange })
+        observer.assertValueAt(1, {
+            val state = it as RewardsLoadedPartialChange
+            state.data.size == 1
+        })
     }
 }
