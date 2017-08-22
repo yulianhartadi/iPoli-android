6f73e8f3b95f173c653007fa0584aba7c5fc29ab
Venelin Valkov <venelin@curiousily.com>
2017-08-22 12:44:47 +0300

    Show completed quests

--- app/src/main/java/io/ipoli/android/quest/overview/DisplayOverviewQuestsUseCase.kt
+++ app/src/main/java/io/ipoli/android/quest/overview/DisplayOverviewQuestsUseCase.kt
@@ -2,14 +2,10 @@ package io.ipoli.android.quest.overview
 
 import io.ipoli.android.common.BaseRxUseCase
 import io.ipoli.android.quest.data.Quest
-import io.ipoli.android.quest.overview.ui.OverviewQuestViewModel
-import io.ipoli.android.quest.overview.ui.OverviewStatePartialChange
-import io.ipoli.android.quest.overview.ui.QuestsLoadedPartialChange
-import io.ipoli.android.quest.overview.ui.QuestsLoadingPartialChange
+import io.ipoli.android.quest.overview.ui.*
 import io.ipoli.android.quest.persistence.QuestRepository
 import io.reactivex.Observable
 import org.threeten.bp.LocalDate
-import timber.log.Timber
 
 /**
  * Created by Venelin Valkov <venelin@curiousily.com>
@@ -20,7 +16,6 @@ class DisplayOverviewQuestsUseCase(private val questRepository: QuestRepository)
     override fun createObservable(params: Parameters): Observable<OverviewStatePartialChange> {
         return questRepository.findScheduledBetween(params.startDate.minusDays(params.showCompletedForPastDays), params.endDate)
             .map { quests ->
-                Timber.d(" Quests size " + quests.size)
                 val comparator = Comparator<Quest> { q1, q2 ->
                     when {
                         q1.scheduledDate!!.isEqual(q2.scheduledDate) -> q1.scheduledDate!!.compareTo(q2.scheduledDate)
@@ -36,15 +31,20 @@ class DisplayOverviewQuestsUseCase(private val questRepository: QuestRepository)
                 val tomorrow = params.today.plusDays(1)
 
                 val (todayQuests, others) = incompleteQuests.partition { it.scheduledDate!!.isEqual(params.today) }
-                val (tomorrowQuests, upcomingQuests) = others.partition { it.scheduledDate!!.isEqual(tomorrow) }
+                val (tomorrowQuests, otherNonCompleted) = others.partition { it.scheduledDate!!.isEqual(tomorrow) }
+
+                val upcomingQuests = otherNonCompleted.filter { it.scheduledDate!!.isAfter(params.today.minusDays(1)) }
 
                 QuestsLoadedPartialChange(toOverviewQuestViewModel(todayQuests),
                     toOverviewQuestViewModel(tomorrowQuests),
                     toOverviewQuestViewModel(upcomingQuests),
-                    toOverviewQuestViewModel(completedQuests)) as OverviewStatePartialChange
+                    toOverviewCompletedQuestViewModel(completedQuests)) as OverviewStatePartialChange
             }
             .startWith(QuestsLoadingPartialChange())
     }
+
+    private fun toOverviewCompletedQuestViewModel(quests: List<Quest>): List<OverviewCompletedQuestViewModel> =
+        quests.map { OverviewCompletedQuestViewModel.create(it) }
 
     private fun toOverviewQuestViewModel(quests: List<Quest>) =
         quests.map { OverviewQuestViewModel.create(it) }
--- app/src/main/java/io/ipoli/android/quest/overview/ui/OverviewController.kt
+++ app/src/main/java/io/ipoli/android/quest/overview/ui/OverviewController.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.PopupMenu
@@ -11,12 +12,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.hannesdorfmann.adapterdelegates3.AdapterDelegate
 import com.hannesdorfmann.adapterdelegates3.AdapterDelegatesManager
 import com.hannesdorfmann.adapterdelegates3.ListDelegationAdapter
 import io.ipoli.android.R
 import io.ipoli.android.common.BaseController
 import io.ipoli.android.common.datetime.Time
+import io.ipoli.android.common.text.DateFormatter
 import io.ipoli.android.common.text.ScheduleTextFormatter
 import io.ipoli.android.daggerComponent
 import io.ipoli.android.quest.data.Quest
@@ -26,11 +29,11 @@ import io.ipoli.android.quest.overview.di.DaggerOverviewComponent
 import io.ipoli.android.quest.overview.di.OverviewComponent
 import io.reactivex.Observable
 import kotlinx.android.synthetic.main.controller_overview.view.*
+import kotlinx.android.synthetic.main.overview_completed_quest_item.view.*
 import kotlinx.android.synthetic.main.overview_quest_item.view.*
 import kotlinx.android.synthetic.main.view_error.view.*
 import kotlinx.android.synthetic.main.view_loading.view.*
 import org.threeten.bp.LocalDate
-import timber.log.Timber
 
 
 /**
@@ -65,8 +68,10 @@ class OverviewController : BaseController<OverviewController, OverviewPresenter>
         questList.setHasFixedSize(true)
         questList.layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
 
-        val delegatesManager = AdapterDelegatesManager<List<OverviewQuestViewModel>>()
-            .addDelegate(OverviewAdapterDelegate(inflater))
+        val delegatesManager = AdapterDelegatesManager<List<Any>>()
+            .addDelegate(HeaderDelegate(inflater))
+            .addDelegate(QuestViewModelDelegate(inflater))
+            .addDelegate(CompletedQuestViewModelDelegate(inflater))
 
         adapter = OverviewAdapter(delegatesManager)
 
@@ -100,29 +105,78 @@ class OverviewController : BaseController<OverviewController, OverviewPresenter>
                 view?.contentView?.visibility = View.GONE
             }
             is OverviewQuestsLoadedState -> {
-                Timber.d("Loaded " + state.todayQuests.size)
                 view?.loadingView?.visibility = View.GONE
                 view?.errorView?.visibility = View.GONE
                 view?.contentView?.visibility = View.VISIBLE
-                adapter.items = state.todayQuests
+
+                val todayList = prependHeaderIfNotEmpty(state.todayQuests, R.string.today)
+                val tomorrowList = prependHeaderIfNotEmpty(state.tomorrowQuests, R.string.tomorrow)
+                val upcomingList = prependHeaderIfNotEmpty(state.upcomingQuests, R.string.next_7_days)
+                val completedList = prependHeaderIfNotEmpty(state.completedQuests, R.string.completed)
+
+                adapter.items = todayList + tomorrowList + upcomingList + completedList
                 adapter.notifyDataSetChanged()
             }
         }
     }
+
+    private fun prependHeaderIfNotEmpty(quests: List<Any>, @StringRes title: Int): List<Any> =
+        if (quests.isNotEmpty()) listOf(HeaderItem(title)) + quests else quests
 }
 
-class OverviewAdapter(manager: AdapterDelegatesManager<List<OverviewQuestViewModel>>) :
-    ListDelegationAdapter<List<OverviewQuestViewModel>>(manager)
+data class HeaderItem(@StringRes val title: Int)
 
-class OverviewAdapterDelegate(private val inflater: LayoutInflater) : AdapterDelegate<List<OverviewQuestViewModel>>() {
+class OverviewAdapter(manager: AdapterDelegatesManager<List<Any>>) :
+    ListDelegationAdapter<List<Any>>(manager)
 
-    override fun onBindViewHolder(items: List<OverviewQuestViewModel>, position: Int, holder: RecyclerView.ViewHolder, payloads: MutableList<Any>) {
-        val vh = holder as QuestViewHolder
-        val questViewModel = items[position]
-        vh.bindQuestViewModel(questViewModel)
+class HeaderDelegate(private val inflater: LayoutInflater) : AdapterDelegate<List<Any>>() {
+    override fun isForViewType(items: List<Any>, position: Int): Boolean =
+        items[position] is HeaderItem
+
+    override fun onCreateViewHolder(parent: ViewGroup?): RecyclerView.ViewHolder =
+        HeaderViewHolder(inflater.inflate(R.layout.overview_header_item, parent, false))
+
+    override fun onBindViewHolder(items: List<Any>, position: Int, holder: RecyclerView.ViewHolder, payloads: MutableList<Any>) {
+        val vh = holder as HeaderDelegate.HeaderViewHolder
+        val headerItem = items[position] as HeaderItem
+        val headerView = vh.itemView as TextView
+        headerView.setText(headerItem.title)
     }
 
-    override fun isForViewType(items: List<OverviewQuestViewModel>, position: Int): Boolean = true
+    inner class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
+
+class CompletedQuestViewModelDelegate(private val inflater: LayoutInflater) : AdapterDelegate<List<Any>>() {
+    override fun isForViewType(items: List<Any>, position: Int): Boolean =
+        items[position] is OverviewCompletedQuestViewModel
+
+    override fun onCreateViewHolder(parent: ViewGroup?): RecyclerView.ViewHolder =
+        CompletedQuestViewModelHolder(inflater.inflate(R.layout.overview_completed_quest_item, parent, false))
+
+    override fun onBindViewHolder(items: List<Any>, position: Int, holder: RecyclerView.ViewHolder, payloads: MutableList<Any>) {
+        val vh = holder as CompletedQuestViewModelHolder
+        val viewModel = items[position] as OverviewCompletedQuestViewModel
+        vh.bind(viewModel)
+    }
+
+    inner class CompletedQuestViewModelHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(viewModel: OverviewCompletedQuestViewModel) {
+            itemView.completedQuestName.text = viewModel.name
+            itemView.completedQuestDueDate.text = DateFormatter.formatWithoutYear(itemView.context, viewModel.dueDate, LocalDate.now())
+        }
+    }
+}
+
+class QuestViewModelDelegate(private val inflater: LayoutInflater) : AdapterDelegate<List<Any>>() {
+
+    override fun onBindViewHolder(items: List<Any>, position: Int, holder: RecyclerView.ViewHolder, payloads: MutableList<Any>) {
+        val vh = holder as QuestViewHolder
+        val questViewModel = items[position]
+        vh.bindQuestViewModel(questViewModel as OverviewQuestViewModel)
+    }
+
+    override fun isForViewType(items: List<Any>, position: Int): Boolean =
+        items[position] is OverviewQuestViewModel
 
     override fun onCreateViewHolder(parent: ViewGroup?): RecyclerView.ViewHolder =
         QuestViewHolder(inflater.inflate(R.layout.overview_quest_item, parent, false))
@@ -137,6 +191,13 @@ class OverviewAdapterDelegate(private val inflater: LayoutInflater) : AdapterDel
                 itemView.name.text = name
                 itemView.scheduleInfo.text = duration.toString()
                 itemView.categoryImage.setImageResource(categoryImage)
+
+                if (isForToday || isForTomorrow) {
+                    itemView.dueDate.visibility = View.GONE
+                } else {
+                    itemView.dueDate.visibility = View.VISIBLE
+                    itemView.dueDate.text = DateFormatter.formatWithoutYear(itemView.context, dueDate, LocalDate.now())
+                }
 
                 if (isStarted) {
                     val drawable = itemView.runningIndicator.background as GradientDrawable
@@ -174,7 +235,6 @@ class OverviewAdapterDelegate(private val inflater: LayoutInflater) : AdapterDel
 
 }
 
-
 interface OverviewStatePartialChange {
     fun computeNewState(prevState: OverviewViewState): OverviewViewState
 }
@@ -182,7 +242,7 @@ interface OverviewStatePartialChange {
 class QuestsLoadedPartialChange(private val todayQuests: List<OverviewQuestViewModel>,
                                 private val tomorrowQuests: List<OverviewQuestViewModel>,
                                 private val upcomingQuests: List<OverviewQuestViewModel>,
-                                private val completedQuests: List<OverviewQuestViewModel>) : OverviewStatePartialChange {
+                                private val completedQuests: List<OverviewCompletedQuestViewModel>) : OverviewStatePartialChange {
     override fun computeNewState(prevState: OverviewViewState) =
         OverviewQuestsLoadedState(todayQuests, tomorrowQuests, upcomingQuests, completedQuests)
 }
@@ -196,7 +256,7 @@ open class OverviewViewState(
     val todayQuests: List<OverviewQuestViewModel> = listOf(),
     val tomorrowQuests: List<OverviewQuestViewModel> = listOf(),
     val upcomingQuests: List<OverviewQuestViewModel> = listOf(),
-    val completedQuests: List<OverviewQuestViewModel> = listOf()
+    val completedQuests: List<OverviewCompletedQuestViewModel> = listOf()
 )
 
 class OverviewLoadingState : OverviewViewState()
@@ -204,20 +264,30 @@ class OverviewLoadingState : OverviewViewState()
 class OverviewQuestsLoadedState(todayQuests: List<OverviewQuestViewModel>,
                                 tomorrowQuests: List<OverviewQuestViewModel>,
                                 upcomingQuests: List<OverviewQuestViewModel>,
-                                completedQuests: List<OverviewQuestViewModel>) :
+                                completedQuests: List<OverviewCompletedQuestViewModel>) :
     OverviewViewState(todayQuests, tomorrowQuests, upcomingQuests, completedQuests)
+
+data class OverviewCompletedQuestViewModel(val name: String,
+                                           val dueDate: LocalDate) {
+    companion object {
+        fun create(quest: Quest): OverviewCompletedQuestViewModel =
+            OverviewCompletedQuestViewModel(quest.name, quest.completedAtDate!!)
+    }
+}
 
 data class OverviewQuestViewModel(
     val name: String,
     val duration: Int,
     val startTime: Time?,
+    val dueDate: LocalDate,
     @DrawableRes val categoryImage: Int,
     @ColorRes val categoryColor: Int,
     val isFromRepeatingQuest: Boolean,
     val isFromChallenge: Boolean,
     val isStarted: Boolean,
     val priority: Int,
-    val isForToday: Boolean
+    val isForToday: Boolean,
+    val isForTomorrow: Boolean
 ) {
     companion object {
         fun create(quest: Quest): OverviewQuestViewModel {
@@ -229,13 +299,15 @@ data class OverviewQuestViewModel(
                 quest.name,
                 quest.getDuration(),
                 startTime,
+                quest.scheduledDate!!,
                 quest.categoryType.colorfulImage,
                 quest.categoryType.color500,
                 quest.isFromRepeatingQuest,
                 quest.isFromChallenge,
                 quest.isStarted,
                 quest.getPriority(),
-                quest.scheduledDate!!.isEqual(LocalDate.now())
+                quest.scheduledDate!!.isEqual(LocalDate.now()),
+                quest.scheduledDate!!.isEqual(LocalDate.now().plusDays(1))
             )
         }
     }
