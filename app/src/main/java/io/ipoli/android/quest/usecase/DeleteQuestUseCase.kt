6575df06fd1103a9cd6a67012f33275f579a08c9
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-10-09 10:46:25 +0300

    Create DeleteQuestUseCase

---
+++ app/src/main/java/io/ipoli/android/quest/usecase/DeleteQuestUseCase.kt
@@ -0,0 +1,16 @@
+package io.ipoli.android.quest.usecase
+
+import io.ipoli.android.common.BaseRxUseCase
+import io.ipoli.android.quest.data.persistence.QuestRepository
+import io.reactivex.Observable
+
+/**
+ * Created by Polina Zhelyazkova <polina@ipoli.io>
+ * on 10/9/17.
+ */
+class DeleteQuestUseCase(private val questRepository: QuestRepository) : BaseRxUseCase<String, Unit>() {
+    override fun createObservable(parameters: String): Observable<Unit> {
+        questRepository.delete(parameters)
+        return Observable.just(Unit)
+    }
+}
