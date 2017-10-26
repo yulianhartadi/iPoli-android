eec6f3f9e5791d55759ce6ce68800ad0761e74b1
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-10-26 22:16:14 +0300

    Hide overlay animation

--- app/src/main/java/io/ipoli/android/reminder/view/ReminderNotificationOverlay.kt
+++ app/src/main/java/io/ipoli/android/reminder/view/ReminderNotificationOverlay.kt
@@ -14,6 +14,8 @@ import android.view.animation.AnticipateInterpolator
 import io.ipoli.android.R
 import io.ipoli.android.common.view.BaseOverlayViewController
 import kotlinx.android.synthetic.main.view_reminder.view.*
+import android.view.ViewAnimationUtils
+
 
 /**
  * Created by Venelin Valkov <venelin@ipoli.io>
@@ -63,14 +65,14 @@ class ReminderNotificationOverlay(private val listener: OnClickListener) {
         windowManager.addView(view, layoutParams)
 
         view.post {
-                        startShowAnimation(view, context)
-//            startHideAnimation(view, context)
+            //                        startShowAnimation(view)
+            startHideAnimation(view)
         }
     }
 
-    data class RevealAnimationProperties(val centerX : Int, val centerY : Int, val radius : Float)
+    data class RevealAnimationProperties(val centerX: Int, val centerY: Int, val radius: Float)
 
-    private fun startShowAnimation(view: ViewGroup, context: Context) {
+    private fun startShowAnimation(view: ViewGroup) {
 
         val props = calculateRevealAnimationProperties(view.backgroundView)
 
@@ -80,37 +82,56 @@ class ReminderNotificationOverlay(private val listener: OnClickListener) {
             0f, props.radius
         )
         anim.interpolator = AnticipateInterpolator(2.0f)
-        anim.duration = context.resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+        anim.duration = view.resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
         anim.start()
 
-        val petAnimator = createPetAnimator(view.pet)
-        val petStateAnimator = createPetAnimator(view.petState)
+        val petAnimator = createShowPetAnimator(view.pet)
+        val petStateAnimator = createShowPetAnimator(view.petState)
         petAnimator.start()
         petStateAnimator.start()
     }
 
-    private fun startHideAnimation(view: ViewGroup, context: Context) {
+    private fun startHideAnimation(view: ViewGroup) {
+        view.pet.visibility = View.VISIBLE
+        view.petState.visibility = View.VISIBLE
 
-
+        val petAnimator = createHidePetAnimator(view.pet)
+        val petStateAnimator = createHidePetAnimator(view.petState)
+        petAnimator.start()
+        petStateAnimator.start()
 
         val props = calculateRevealAnimationProperties(view.backgroundView)
-
-        val anim = ViewAnimationUtils.createCircularReveal(
+        val backgroundAnim = ViewAnimationUtils.createCircularReveal(
             view.backgroundView,
             props.centerX, props.centerY,
             props.radius, 0f
         )
-        anim.interpolator = AnticipateInterpolator(2.0f)
-        anim.duration = context.resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
-        anim.addListener(object : AnimatorListenerAdapter() {
+        backgroundAnim.interpolator = AccelerateDecelerateInterpolator()
+//        anim.duration = view.resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
+        backgroundAnim.duration = 2000
+        backgroundAnim.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
                 view.backgroundView.visibility = View.INVISIBLE
             }
         })
-        anim.start()
+
+
+        val views = listOf<View>(view.dismiss, view.snooze, view.done,
+            view.dismissHint, view.snoozeHint, view.doneHint,
+            view.name, view.message, view.time)
+        val animators = views.map { ObjectAnimator.ofFloat(it, "alpha", 1f, 0f).setDuration(1000) }
+            .toMutableList() as MutableList<Animator>
+        animators.add(backgroundAnim)
+
+        val set = AnimatorSet()
+        set.playTogether(animators)
+        set.startDelay = 500
+        set.start()
+
+
     }
 
-    private fun calculateRevealAnimationProperties(view : View) : RevealAnimationProperties {
+    private fun calculateRevealAnimationProperties(view: View): RevealAnimationProperties {
         val bounds = Rect()
         view.getDrawingRect(bounds)
 
@@ -132,7 +153,7 @@ class ReminderNotificationOverlay(private val listener: OnClickListener) {
         return metrics.heightPixels
     }
 
-    private fun createPetAnimator(view: View): ObjectAnimator {
+    private fun createShowPetAnimator(view: View): ObjectAnimator {
         val animator = ObjectAnimator.ofFloat(view, "y", view.y + view.height, view.y)
         animator.duration = view.context.resources.getInteger(android.R.integer.config_longAnimTime).toLong()
         animator.interpolator = AccelerateDecelerateInterpolator()
@@ -141,6 +162,21 @@ class ReminderNotificationOverlay(private val listener: OnClickListener) {
 
             override fun onAnimationStart(animation: Animator?) {
                 view.visibility = View.VISIBLE
+            }
+        })
+
+        return animator
+    }
+
+    private fun createHidePetAnimator(view: View): ObjectAnimator {
+        val animator = ObjectAnimator.ofFloat(view.pet, "y", view.y, view.y + view.height)
+//        animator.duration = view.context.resources.getInteger(android.R.integer.config_longAnimTime).toLong()
+        animator.duration = 1000
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.addListener(object : AnimatorListenerAdapter() {
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.visibility = View.INVISIBLE
             }
         })
 
