895187b92ff5d2ce8f953cdde780a140a3375df8
Venelin Valkov <venelin@curiousily.com>
2017-11-14 22:08:59 +0200

    Add required XP for level generator

---
+++ app/src/main/java/io/ipoli/android/player/ExperienceForLevelGenerator.kt
@@ -0,0 +1,29 @@
+package io.ipoli.android.player
+
+/**
+ * Created by Venelin Valkov <venelin@ipoli.io>
+ * on 11/14/17.
+ */
+object ExperienceForLevelGenerator {
+
+    private const val SWITCH_LEVEL = 6
+    private const val LARGE_INCREASE = 80
+    private const val SMALL_INCREASE = 50
+
+    fun forLevel(level: Int): Long {
+
+        if (level <= 1) {
+            return 0
+        }
+
+        if (level == SWITCH_LEVEL) {
+            val xpPrevLvl = forLevel(level - 1)
+            val prevLevelDif = xpPrevLvl - forLevel(level - 2)
+            return xpPrevLvl + prevLevelDif + LARGE_INCREASE * 2
+        }
+
+        val c = if (level < SWITCH_LEVEL) SMALL_INCREASE else LARGE_INCREASE
+
+        return (c * (level - 1)) + forLevel(level - 1)
+    }
+}
