4777a81864999d0121acd7dea1f3ca4a98756910
Venelin Valkov <venelin@curiousily.com>
2017-12-06 16:35:19 +0200

    Show reward in QuestCompletePopup

--- app/src/main/java/io/ipoli/android/home/HomeViewController.kt
+++ app/src/main/java/io/ipoli/android/home/HomeViewController.kt
@@ -17,7 +17,7 @@ import io.ipoli.android.common.mvi.MviViewController
 import io.ipoli.android.common.mvi.ViewStateRenderer
 import io.ipoli.android.common.view.FeedbackDialogController
 import io.ipoli.android.pet.PetViewController
-import io.ipoli.android.pet.shop.PetShopViewController
+import io.ipoli.android.quest.calendar.CalendarViewController
 import kotlinx.android.synthetic.main.controller_home.view.*
 import org.json.JSONObject
 import space.traversal.kapsule.Injects
@@ -88,8 +88,8 @@ class HomeViewController(args: Bundle? = null) :
         val handler = FadeChangeHandler()
         val childRouter = getChildRouter(view.controllerContainer, null)
         childRouter.setRoot(
-//            RouterTransaction.with(CalendarViewController())
-            RouterTransaction.with(PetShopViewController())
+            RouterTransaction.with(CalendarViewController())
+//            RouterTransaction.with(PetShopViewController())
 //            RouterTransaction.with(PetViewController())
                 .pushChangeHandler(handler)
                 .popChangeHandler(handler)
--- app/src/main/java/io/ipoli/android/quest/view/QuestCompletePopup.kt
+++ app/src/main/java/io/ipoli/android/quest/view/QuestCompletePopup.kt
@@ -1,27 +1,37 @@
 package io.ipoli.android.quest.view
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ValueAnimator
+import android.animation.*
+import android.support.transition.AutoTransition
+import android.support.transition.Transition
+import android.support.transition.TransitionListenerAdapter
+import android.support.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import io.ipoli.android.R
 import io.ipoli.android.common.view.BasePopup
 import io.ipoli.android.common.view.anim.TypewriterTextAnimator
 import io.ipoli.android.common.view.visible
+import io.ipoli.android.pet.Food
 import kotlinx.android.synthetic.main.popup_quest_complete.view.*
-
 
 class QuestCompletePopup(
     private val earnedXP: Int,
-    private val earnedCoins: Int
-    ) : BasePopup(isAutoHide = true) {
+    private val earnedCoins: Int,
+    private val reward: FoodReward? = null
+) : BasePopup(isAutoHide = true) {
+
+    data class FoodReward(val food: Food, val quantity: Int = 1)
 
     override fun createView(inflater: LayoutInflater): View =
         inflater.inflate(R.layout.popup_quest_complete, null)
 
     override fun onViewShown(contentView: View) {
+        reward?.let {
+            contentView.reward.setImageResource(it.food.image)
+            contentView.rewardQuantity.text = "x${it.quantity}"
+        }
         startTypingAnimation(contentView)
     }
 
@@ -60,10 +70,48 @@ class QuestCompletePopup(
 
         anim.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
-                autoHideAfter(700)
+                if (reward != null) playRewardAnimation(contentView)
+                else autoHideAfter(700)
             }
         })
 
         anim.start()
+    }
+
+    private fun playRewardAnimation(contentView: View) {
+        val transition = AutoTransition()
+        transition.addListener(object : TransitionListenerAdapter() {
+
+            override fun onTransitionEnd(transition: Transition) {
+                val xAnim = ObjectAnimator.ofFloat(contentView.reward, "scaleX", 0f, 1f)
+                val yAnim = ObjectAnimator.ofFloat(contentView.reward, "scaleY", 0f, 1f)
+                val set = AnimatorSet()
+                set.interpolator = OvershootInterpolator()
+                set.playTogether(xAnim, yAnim)
+                set.addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationStart(animation: Animator?) {
+                        contentView.reward.visible = true
+                        contentView.rewardQuantity.visible = true
+                    }
+
+                    override fun onAnimationEnd(animation: Animator?) {
+                        val fadeAnim = ObjectAnimator.ofFloat(contentView.rewardQuantity, "alpha", 0f, 1f)
+                        fadeAnim.addListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator?) {
+                                autoHideAfter(700)
+                            }
+                        })
+                        fadeAnim.start()
+                    }
+                })
+                set.start()
+            }
+        })
+
+
+        TransitionManager.beginDelayedTransition(contentView as ViewGroup, transition)
+        contentView.reward.visible = false
+        contentView.rewardQuantity.visible = false
     }
 }
