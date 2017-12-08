cfd757301b109f3c81de7733a70ec0ed433dca3b
Polina Zhelyazkova <poly_vjk@abv.bg>
2017-12-08 14:15:00 +0200

    Display pet head in text picker

--- app/src/main/java/io/ipoli/android/common/view/TextPickerDialogController.kt
+++ app/src/main/java/io/ipoli/android/common/view/TextPickerDialogController.kt
@@ -1,19 +1,33 @@
 package io.ipoli.android.common.view
 
-import android.app.Dialog
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.view.View
 import io.ipoli.android.R
+import io.ipoli.android.common.di.ControllerModule
+import io.ipoli.android.common.mvi.ViewStateRenderer
+import io.ipoli.android.pet.AndroidPetAvatar
+import io.ipoli.android.pet.LoadPetIntent
+import io.ipoli.android.pet.PetDialogPresenter
+import io.ipoli.android.pet.PetDialogViewState
 import kotlinx.android.synthetic.main.dialog_text_picker.view.*
+import space.traversal.kapsule.Injects
+import space.traversal.kapsule.required
 
 /**
  * Created by Polina Zhelyazkova <polina@ipoli.io>
  * on 12/3/17.
  */
-class TextPickerDialogController : BaseDialogController {
+class TextPickerDialogController : MviDialogController<PetDialogViewState, ColorPickerDialogController, PetDialogPresenter, LoadPetIntent>
+    , ViewStateRenderer<PetDialogViewState>, Injects<ControllerModule> {
+
+    private val presenter by required { petDialogPresenter }
+
+    override fun createPresenter() = presenter
 
     private var listener: (String) -> Unit = {}
+
     private var title: String = ""
     private var text: String = ""
     private var hint: String = ""
@@ -27,8 +41,7 @@ class TextPickerDialogController : BaseDialogController {
 
     constructor(args: Bundle? = null) : super(args)
 
-
-    override fun onCreateDialog(savedViewState: Bundle?): Dialog {
+    override fun onCreateDialog(savedViewState: Bundle?): DialogView {
         val inflater = LayoutInflater.from(activity!!)
 
         val contentView = inflater.inflate(R.layout.dialog_text_picker, null)
@@ -39,7 +52,7 @@ class TextPickerDialogController : BaseDialogController {
 
         contentView.textLayout.hint = hint
 
-        return AlertDialog.Builder(activity!!)
+        val dialog = AlertDialog.Builder(activity!!)
             .setView(contentView)
             .setTitle(title)
             .setIcon(R.drawable.pet_5_head)
@@ -48,6 +61,18 @@ class TextPickerDialogController : BaseDialogController {
             })
             .setNegativeButton(R.string.cancel, null)
             .create()
+
+        return DialogView(dialog, contentView)
     }
 
+    override fun onAttach(view: View) {
+        super.onAttach(view)
+        send(LoadPetIntent)
+    }
+
+    override fun render(state: PetDialogViewState, view: View) {
+        if (state.type == PetDialogViewState.Type.PET_LOADED) {
+            changeIcon(AndroidPetAvatar.valueOf(state.petAvatar!!.name).headImage)
+        }
+    }
 }
