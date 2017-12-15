385a38f37561f42a9479c16aae0cf20cee7841ba
Venelin Valkov <venelin@curiousily.com>
2017-12-15 14:24:05 +0200

    Fix authide in Popup

--- app/src/main/java/io/ipoli/android/common/view/PetMessagePopup.kt
+++ app/src/main/java/io/ipoli/android/common/view/PetMessagePopup.kt
@@ -11,18 +11,23 @@ class PetMessagePopup(
     private val message: String,
     private val avatar: AndroidPetAvatar,
     private val undoListener: () -> Unit
-) : BasePopup(position = Position.BOTTOM) {
+) : BasePopup(position = Position.BOTTOM, isAutoHide = true) {
 
-    override fun createView(inflater: LayoutInflater): View =
-        inflater.inflate(R.layout.popup_pet_message, null)
+    override fun createView(inflater: LayoutInflater): View {
+        val v = inflater.inflate(R.layout.popup_pet_message, null)
+        v.petMessage.text = message
+        v.petHead.setImageResource(avatar.headImage)
 
-    override fun onViewShown(contentView: View) {
-        contentView.petMessage.text = message
-        contentView.petHead.setImageResource(avatar.headImage)
-        contentView.undoAction.setOnClickListener {
+        v.undoAction.setOnClickListener {
             undoListener()
             hide()
         }
+
+        return v
+    }
+
+
+    override fun onViewShown(contentView: View) {
         autoHideAfter(TimeUnit.SECONDS.toMillis(2))
     }
 }
