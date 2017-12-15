9c8496ce3335e7f9830eb04e839ab534e70dd2a8
Venelin Valkov <venelin@curiousily.com>
2017-12-15 16:59:17 +0200

    Lock day change in edit mode

--- app/src/main/java/io/ipoli/android/quest/calendar/CalendarViewController.kt
+++ app/src/main/java/io/ipoli/android/quest/calendar/CalendarViewController.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
-import android.support.v7.widget.Toolbar
 import android.view.*
 import android.view.animation.AccelerateDecelerateInterpolator
 import com.bluelinelabs.conductor.Controller
@@ -398,9 +397,11 @@ class CalendarViewController(args: Bundle? = null) :
 
     fun onStartEdit() {
         view!!.addQuest.visible = false
+        view!!.pager.isLocked = true
     }
 
     fun onStopEdit() {
         view!!.addQuest.visible = true
+        view!!.pager.isLocked = false
     }
 }
