1718eac748f94ac8d001257dd7c43c56c92a457a
Venelin Valkov <venelin@curiousily.com>
2017-12-18 17:55:10 +0200

    Fix scheduling next reminder

--- app/src/main/java/io/ipoli/android/quest/usecase/RemoveQuestUseCase.kt
+++ app/src/main/java/io/ipoli/android/quest/usecase/RemoveQuestUseCase.kt
@@ -1,10 +1,8 @@
 package io.ipoli.android.quest.usecase
 
 import io.ipoli.android.common.UseCase
-import io.ipoli.android.common.datetime.DateUtils
 import io.ipoli.android.quest.data.persistence.QuestRepository
 import io.ipoli.android.reminder.ReminderScheduler
-import org.threeten.bp.LocalDate
 
 /**
  * Created by Polina Zhelyazkova <polina@ipoli.io>
@@ -16,7 +14,7 @@ class RemoveQuestUseCase(private val questRepository: QuestRepository, private v
             return
         }
         questRepository.remove(parameters)
-        val quests = questRepository.findNextQuestsToRemind(DateUtils.toMillis(LocalDate.now()))
+        val quests = questRepository.findNextQuestsToRemind()
         if (quests.isNotEmpty()) {
             val reminder = quests[0].reminder!!
             reminderScheduler.schedule(reminder.toMillis())
