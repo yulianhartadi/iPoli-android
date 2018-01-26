ff6abb111c62d9a4e494cce99ae8e3e4a52becd5
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-01-26 15:09:20 +0200

    Add simple recycler view with simple view holder

--- app/src/main/java/mypoli/android/quest/agenda/AgendaViewController.kt
+++ app/src/main/java/mypoli/android/quest/agenda/AgendaViewController.kt
@@ -1,10 +1,14 @@
 package mypoli.android.quest.agenda
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.RestoreViewOnCreateController
+import kotlinx.android.synthetic.main.controller_agenda.view.*
+import kotlinx.android.synthetic.main.item_agenda_quest.view.*
 import mypoli.android.R
 
 /**
@@ -18,7 +22,48 @@ class AgendaViewController : RestoreViewOnCreateController() {
         savedViewState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.controller_agenda, container, false)
+        view.agendaList.layoutManager =
+            LinearLayoutManager(container.context, LinearLayoutManager.VERTICAL, false)
+        view.agendaList.adapter = QuestAdapter(listOf(
+            QuestViewModel("Run"),
+            QuestViewModel("Walk"),
+            QuestViewModel("Dance")
+        ))
         return view
     }
+
+    data class QuestViewModel(
+        val name: String
+    )
+
+    inner class QuestAdapter(private var viewModels: List<QuestViewModel> = listOf()) :
+        RecyclerView.Adapter<QuestAdapter.ViewHolder>() {
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val vm = viewModels[position]
+            val itemView = holder.itemView
+
+            itemView.questName.text = vm.name
+        }
+
+        override fun getItemCount() = viewModels.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_agenda_quest,
+                    parent,
+                    false
+                )
+            )
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        fun updateAll(viewModels: List<QuestViewModel>) {
+            this.viewModels = viewModels
+            notifyDataSetChanged()
+        }
+
+    }
+
 
 }
