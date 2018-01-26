9bb05142d3fe19b1d9e645fbdf9103b19a127549
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-01-26 16:02:17 +0200

    Design item

--- app/src/main/java/mypoli/android/quest/agenda/AgendaViewController.kt
+++ app/src/main/java/mypoli/android/quest/agenda/AgendaViewController.kt
@@ -1,5 +1,6 @@
 package mypoli.android.quest.agenda
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -7,9 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.RestoreViewOnCreateController
+import com.mikepenz.iconics.IconicsDrawable
 import kotlinx.android.synthetic.main.controller_agenda.view.*
 import kotlinx.android.synthetic.main.item_agenda_quest.view.*
 import mypoli.android.R
+import mypoli.android.common.datetime.Time
+import mypoli.android.common.view.AndroidColor
+import mypoli.android.common.view.AndroidIcon
+import mypoli.android.common.view.colorRes
 
 /**
  * Created by Polina Zhelyazkova <polina@ipoli.io>
@@ -24,16 +30,21 @@ class AgendaViewController : RestoreViewOnCreateController() {
         val view = inflater.inflate(R.layout.controller_agenda, container, false)
         view.agendaList.layoutManager =
             LinearLayoutManager(container.context, LinearLayoutManager.VERTICAL, false)
-        view.agendaList.adapter = QuestAdapter(listOf(
-            QuestViewModel("Run"),
-            QuestViewModel("Walk"),
-            QuestViewModel("Dance")
-        ))
+        view.agendaList.adapter = QuestAdapter(
+            listOf(
+                QuestViewModel("Run", Time.Companion.at(10, 30), AndroidColor.BLUE, AndroidIcon.ACADEMIC),
+                QuestViewModel("Walk", Time.Companion.at(20, 30), AndroidColor.DEEP_ORANGE, AndroidIcon.BIKE),
+                QuestViewModel("Dance", Time.Companion.at(8, 30), AndroidColor.GREEN, AndroidIcon.BUS)
+            )
+        )
         return view
     }
 
     data class QuestViewModel(
-        val name: String
+        val name: String,
+        val startTime: Time,
+        val color: AndroidColor,
+        val icon: AndroidIcon
     )
 
     inner class QuestAdapter(private var viewModels: List<QuestViewModel> = listOf()) :
@@ -43,6 +54,15 @@ class AgendaViewController : RestoreViewOnCreateController() {
             val itemView = holder.itemView
 
             itemView.questName.text = vm.name
+            itemView.questIcon.backgroundTintList =
+                ColorStateList.valueOf(colorRes(vm.color.color500))
+            itemView.questIcon.setImageDrawable(
+                IconicsDrawable(itemView.context)
+                    .icon(vm.icon.icon)
+                    .colorRes(R.color.md_white)
+                    .sizeDp(24)
+            )
+            itemView.questStartTime.text = vm.startTime.toString()
         }
 
         override fun getItemCount() = viewModels.size
