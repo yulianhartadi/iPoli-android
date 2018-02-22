f0d075ad3b1c1a3e2515d73cfbb0c128ff7409b7
Venelin Valkov <venelin@curiousily.com>
2018-02-22 17:56:38 +0200

    Show progress

--- app/src/main/java/mypoli/android/repeatingquest/show/RepeatingQuestViewController.kt
+++ app/src/main/java/mypoli/android/repeatingquest/show/RepeatingQuestViewController.kt
@@ -1,6 +1,9 @@
 package mypoli.android.repeatingquest.show
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.support.annotation.ColorInt
+import android.support.annotation.LayoutRes
 import android.support.v4.view.ViewCompat
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +11,9 @@ import android.view.ViewGroup
 import kotlinx.android.synthetic.main.controller_repeating_quest.view.*
 import mypoli.android.MainActivity
 import mypoli.android.R
+import mypoli.android.common.ViewUtils
 import mypoli.android.common.redux.android.ReduxViewController
-import mypoli.android.common.view.AndroidColor
-import mypoli.android.common.view.colorRes
-import mypoli.android.common.view.setToolbar
-import mypoli.android.common.view.toolbarTitle
+import mypoli.android.common.view.*
 
 
 /**
@@ -70,6 +71,92 @@ class RepeatingQuestViewController :
                 toolbarTitle = state.name
                 view.questName.text = state.name
                 colorLayout(view, state)
+
+                val inflater = LayoutInflater.from(view.context)
+                view.progressContainer.removeAllViews()
+
+                for (vm in state.progressViewModels) {
+                    val progressViewEmpty = inflater.inflate(
+                        vm.layout,
+                        view.progressContainer,
+                        false
+                    )
+                    val progressViewEmptyBackground =
+                        progressViewEmpty.background as GradientDrawable
+                    progressViewEmptyBackground.setStroke(
+                        ViewUtils.dpToPx(1.5f, view.context).toInt(),
+                        vm.color
+                    )
+
+                    progressViewEmptyBackground.setColor(vm.color)
+
+                    view.progressContainer.addView(progressViewEmpty)
+                }
+
+
+//                val totalCount = currentPeriodHistory.getTotalCount()
+//                val completedCount = currentPeriodHistory.getCompletedCount()
+//                if (totalCount > 7) {
+//                    val progressText = inflater.inflate(
+//                        R.layout.repeating_quest_progress_text,
+//                        progressContainer,
+//                        false
+//                    ) as TextView
+//                    progressText.setText(
+//                        getString(
+//                            R.string.repeating_quest_completed_this_month,
+//                            completedCount
+//                        )
+//                    )
+//                    progressContainer.addView(progressText)
+//                    return
+//                }
+//
+//                val incomplete = currentPeriodHistory.getRemainingCount()
+//
+//                var progressColor = R.color.colorAccent
+//
+//                if (category == Category.WORK || category == Category.FUN || category == Category.CHORES) {
+//                    progressColor = R.color.colorAccentAlternative
+//                }
+//
+//                for (i in 1..completedCount) {
+//                    val progressViewEmpty = inflater.inflate(
+//                        R.layout.repeating_quest_progress_indicator_empty,
+//                        progressContainer,
+//                        false
+//                    )
+//                    val progressViewEmptyBackground =
+//                        progressViewEmpty.getBackground() as GradientDrawable
+//
+//                    progressViewEmptyBackground.setStroke(
+//                        ViewUtils.dpToPx(1.5f, resources) as Int,
+//                        ContextCompat.getColor(this, progressColor)
+//                    )
+//                    progressViewEmptyBackground.setColor(
+//                        ContextCompat.getColor(
+//                            this,
+//                            progressColor
+//                        )
+//                    )
+//                    progressContainer.addView(progressViewEmpty)
+//                }
+//
+//                for (i in 1..incomplete) {
+//                    val progressViewEmpty = inflater.inflate(
+//                        R.layout.repeating_quest_progress_indicator_empty,
+//                        progressContainer,
+//                        false
+//                    )
+//                    val progressViewEmptyBackground =
+//                        progressViewEmpty.getBackground() as GradientDrawable
+//                    progressViewEmptyBackground.setStroke(
+//                        ViewUtils.dpToPx(1.5f, resources) as Int,
+//                        Color.WHITE
+//                    )
+//                    progressViewEmptyBackground.setColor(Color.WHITE)
+//                    progressContainer.addView(progressViewEmpty)
+//                }
             }
         }
     }
@@ -90,4 +177,25 @@ class RepeatingQuestViewController :
 
     private val RepeatingQuestViewState.Changed.color700
         get() = AndroidColor.valueOf(color.name).color700
+
+    private val RepeatingQuestViewState.Changed.progressViewModels
+        get() = progress.map {
+            when (it) {
+                RepeatingQuestViewState.Changed.ProgressModel.COMPLETE -> {
+                    ProgressViewModel(
+                        R.layout.repeating_quest_progress_indicator_empty,
+                        attrData(R.attr.colorAccent)
+                    )
+                }
+
+                RepeatingQuestViewState.Changed.ProgressModel.INCOMPLETE -> {
+                    ProgressViewModel(
+                        R.layout.repeating_quest_progress_indicator_empty,
+                        colorRes(R.color.md_white)
+                    )
+                }
+            }
+        }
+
+    data class ProgressViewModel(@LayoutRes val layout: Int, @ColorInt val color: Int)
 }
--- app/src/main/java/mypoli/android/repeatingquest/show/RepeatingQuestViewState.kt
+++ app/src/main/java/mypoli/android/repeatingquest/show/RepeatingQuestViewState.kt
@@ -2,9 +2,15 @@ package mypoli.android.repeatingquest.show
 
 import mypoli.android.common.AppState
 import mypoli.android.common.BaseViewStateReducer
+import mypoli.android.common.datetime.Duration
+import mypoli.android.common.datetime.Minute
+import mypoli.android.common.datetime.minutes
 import mypoli.android.common.mvi.ViewState
 import mypoli.android.common.redux.Action
 import mypoli.android.quest.Color
+import mypoli.android.repeatingquest.show.RepeatingQuestViewState.Changed.ProgressModel.COMPLETE
+import mypoli.android.repeatingquest.show.RepeatingQuestViewState.Changed.ProgressModel.INCOMPLETE
+import org.threeten.bp.LocalDate
 
 /**
  * Created by Venelin Valkov <venelin@mypoli.fun>
@@ -19,7 +25,18 @@ sealed class RepeatingQuestViewState : ViewState {
 
     object Loading : RepeatingQuestViewState()
 
-    data class Changed(val name: String, val color: Color) : RepeatingQuestViewState()
+    data class Changed(
+        val name: String,
+        val color: Color,
+        val nextScheduledDate: LocalDate,
+        val totalDuration: Duration<Minute>,
+        val currentStreak: Int,
+        val progress: List<ProgressModel>
+    ) : RepeatingQuestViewState() {
+        enum class ProgressModel {
+            COMPLETE, INCOMPLETE
+        }
+    }
 }
 
 object RepeatingQuestReducer : BaseViewStateReducer<RepeatingQuestViewState>() {
@@ -32,7 +49,14 @@ object RepeatingQuestReducer : BaseViewStateReducer<RepeatingQuestViewState>() {
         action: Action
     ) = when (action) {
         is RepeatingQuestAction.Load -> {
-            RepeatingQuestViewState.Changed("Hello World", Color.BLUE)
+            RepeatingQuestViewState.Changed(
+                name = "Hello World",
+                color = Color.TEAL,
+                nextScheduledDate = LocalDate.now(),
+                totalDuration = 180.minutes,
+                currentStreak = 10,
+                progress = listOf(COMPLETE, COMPLETE, COMPLETE, INCOMPLETE, INCOMPLETE)
+            )
         }
         else -> subState
     }
