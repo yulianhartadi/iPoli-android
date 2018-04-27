9301df7b548a3ae9f36ed328f75eb4b0dccd5b35
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-04-27 13:36:22 +0300

    Redesign auth

--- app/src/main/java/io/ipoli/android/player/auth/AuthViewController.kt
+++ app/src/main/java/io/ipoli/android/player/auth/AuthViewController.kt
@@ -6,29 +6,37 @@ import android.animation.AnimatorSet
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
 import android.support.design.widget.Snackbar
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import com.bluelinelabs.conductor.RouterTransaction
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.crashlytics.android.Crashlytics
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.mikepenz.google_material_typeface_library.GoogleMaterial
-import com.mikepenz.iconics.IconicsDrawable
-import kotlinx.android.synthetic.main.controller_auth.view.*
 import io.ipoli.android.R
-import io.ipoli.android.player.auth.error.SignInError
 import io.ipoli.android.common.EmailUtils
 import io.ipoli.android.common.LoaderDialogController
 import io.ipoli.android.common.ViewUtils
+import io.ipoli.android.common.home.HomeViewController
 import io.ipoli.android.common.redux.android.ReduxViewController
 import io.ipoli.android.common.view.*
-import io.ipoli.android.common.home.HomeViewController
+import io.ipoli.android.common.view.recyclerview.BaseRecyclerViewAdapter
+import io.ipoli.android.common.view.recyclerview.SimpleViewHolder
+import io.ipoli.android.player.auth.error.SignInError
+import io.ipoli.android.player.data.AndroidAvatar
+import io.ipoli.android.player.data.Avatar
+import kotlinx.android.synthetic.main.controller_auth.view.*
+import kotlinx.android.synthetic.main.item_auth_avatar.view.*
 
 
 /**
@@ -53,15 +61,64 @@ class AuthViewController(args: Bundle? = null) :
 
         registerForActivityResult(RC_SIGN_IN)
 
-        view.switchToLogin.dispatchOnClick(AuthAction.SwitchViewType)
-        view.closeLogin.dispatchOnClick(AuthAction.SwitchViewType)
+        Glide.with(view.context).load(AndroidAvatar.AVATAR_03.image)
+            .apply(RequestOptions.circleCropTransform())
+            .into(view.playerAvatar)
 
-        view.switchToLogin.setImageDrawable(
-            IconicsDrawable(view.context)
-                .icon(GoogleMaterial.Icon.gmd_vpn_key)
-                .colorRes(R.color.md_white)
-                .sizeDp(24)
+        view.avatarList.layoutManager =
+            LinearLayoutManager(activity!!, LinearLayoutManager.HORIZONTAL, false)
+        view.avatarList.adapter = AvatarAdapter()
+
+        (view.avatarList.adapter as AvatarAdapter).updateAll(
+            listOf(
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_03.avatarName),
+                    AndroidAvatar.AVATAR_03.image,
+                    AndroidAvatar.AVATAR_03.backgroundColor,
+                    Avatar.AVATAR_03
+                ),
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_04.avatarName),
+                    AndroidAvatar.AVATAR_04.image,
+                    AndroidAvatar.AVATAR_04.backgroundColor,
+                    Avatar.AVATAR_04
+                ),
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_05.avatarName),
+                    AndroidAvatar.AVATAR_05.image,
+                    AndroidAvatar.AVATAR_05.backgroundColor,
+                    Avatar.AVATAR_05
+                ),
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_06.avatarName),
+                    AndroidAvatar.AVATAR_06.image,
+                    AndroidAvatar.AVATAR_06.backgroundColor,
+                    Avatar.AVATAR_06
+                ),
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_07.avatarName),
+                    AndroidAvatar.AVATAR_07.image,
+                    AndroidAvatar.AVATAR_07.backgroundColor,
+                    Avatar.AVATAR_07
+                ),
+                AvatarViewModel(
+                    stringRes(AndroidAvatar.AVATAR_08.avatarName),
+                    AndroidAvatar.AVATAR_08.image,
+                    AndroidAvatar.AVATAR_08.backgroundColor,
+                    Avatar.AVATAR_08
+                )
+            )
         )
+
+//        view.switchToLogin.dispatchOnClick(AuthAction.SwitchViewType)
+//        view.closeLogin.dispatchOnClick(AuthAction.SwitchViewType)
+//
+//        view.switchToLogin.setImageDrawable(
+//            IconicsDrawable(view.context)
+//                .icon(GoogleMaterial.Icon.gmd_vpn_key)
+//                .colorRes(R.color.md_white)
+//                .sizeDp(24)
+//        )
         return view
     }
 
@@ -80,63 +137,72 @@ class AuthViewController(args: Bundle? = null) :
 
             AuthViewState.StateType.SHOW_SIGN_UP -> {
 
-                if (view.loginContainer.visible) {
-                    playShowSignUpAnimation(view, {
-                        renderSighUpViews(view, state)
-                    })
-                } else {
+//                if (view.loginContainer.visible) {
+//                    playShowSignUpAnimation(view, {
+//                        renderSighUpViews(view, state)
+//                    })
+//                } else {
                     renderSighUpViews(view, state)
-                }
-
-                view.googleSignIn.setOnClickListener {
-                    dispatch(
-                        AuthAction.SignUp(
-                            view.username.text.toString(),
-                            AuthViewState.Provider.GOOGLE
-                        )
-                    )
-                }
-                view.facebookSignIn.setOnClickListener {
-                    dispatch(
-                        AuthAction.SignUp(
-                            view.username.text.toString(),
-                            AuthViewState.Provider.FACEBOOK
-                        )
-                    )
-                }
+//                }
 
                 view.anonymousSignUp.setOnClickListener {
-                    dispatch(
-                        AuthAction.SignUp(
-                            view.username.text.toString(),
-                            AuthViewState.Provider.GUEST
-                        )
-                    )
+                    playShowLoginAnimation(view, {
+                        view.loginContainer.visibility = View.INVISIBLE
+                        view.loginPet.visible = false
+                        view.setupPet.visible = true
+//                        view.signUpHeadline.setText(R.string.welcome_back_hero)
+                    })
                 }
+
+//                view.googleSignIn.setOnClickListener {
+//                    dispatch(
+//                        AuthAction.SignUp(
+//                            view.username.text.toString(),
+//                            AuthViewState.Provider.GOOGLE
+//                        )
+//                    )
+//                }
+//                view.facebookSignIn.setOnClickListener {
+//                    dispatch(
+//                        AuthAction.SignUp(
+//                            view.username.text.toString(),
+//                            AuthViewState.Provider.FACEBOOK
+//                        )
+//                    )
+//                }
+//
+//                view.anonymousSignUp.setOnClickListener {
+//                    dispatch(
+//                        AuthAction.SignUp(
+//                            view.username.text.toString(),
+//                            AuthViewState.Provider.GUEST
+//                        )
+//                    )
+//                }
             }
 
             AuthViewState.StateType.SHOW_LOGIN -> {
                 playShowLoginAnimation(view, {
-                    view.signUpContainer.visibility = View.INVISIBLE
-                    view.signUpPet.visible = false
-                    view.loginPet.visible = true
-                    view.signUpHeadline.setText(R.string.welcome_back_hero)
+                    view.loginContainer.visibility = View.INVISIBLE
+                    view.loginPet.visible = false
+                    view.setupPet.visible = true
+//                    view.signUpHeadline.setText(R.string.welcome_back_hero)
                 })
 
-                view.googleLogin.setOnClickListener {
-                    dispatch(
-                        AuthAction.Login(
-                            AuthViewState.Provider.GOOGLE
-                        )
-                    )
-                }
-                view.facebookLogin.setOnClickListener {
-                    dispatch(
-                        AuthAction.Login(
-                            AuthViewState.Provider.FACEBOOK
-                        )
-                    )
-                }
+//                view.googleLogin.setOnClickListener {
+//                    dispatch(
+//                        AuthAction.Login(
+//                            AuthViewState.Provider.GOOGLE
+//                        )
+//                    )
+//                }
+//                view.facebookLogin.setOnClickListener {
+//                    dispatch(
+//                        AuthAction.Login(
+//                            AuthViewState.Provider.FACEBOOK
+//                        )
+//                    )
+//                }
             }
 
             AuthViewState.StateType.GOOGLE_AUTH_STARTED -> {
@@ -159,12 +225,12 @@ class AuthViewController(args: Bundle? = null) :
                 showLoader()
                 FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener {
                     if (it.isSuccessful) {
-                        dispatch(
-                            AuthAction.CompleteUserAuth(
-                                FirebaseAuth.getInstance().currentUser!!,
-                                view.username.text.toString()
-                            )
-                        )
+//                        dispatch(
+//                            AuthAction.CompleteUserAuth(
+//                                FirebaseAuth.getInstance().currentUser!!,
+//                                view.username.text.toString()
+//                            )
+//                        )
                     } else {
                         hideLoader()
                         showShortToast(R.string.something_went_wrong)
@@ -175,7 +241,7 @@ class AuthViewController(args: Bundle? = null) :
 
             AuthViewState.StateType.USERNAME_VALIDATION_ERROR -> {
                 hideLoader()
-                view.username.error = state.usernameErrorMessage(activity!!)
+//                view.username.error = state.usernameErrorMessage(activity!!)
             }
 
             AuthViewState.StateType.PLAYER_CREATED -> {
@@ -261,13 +327,13 @@ class AuthViewController(args: Bundle? = null) :
     }
 
     private fun renderSighUpViews(view: View, state: AuthViewState) {
-        view.loginContainer.visibility = View.INVISIBLE
-        if (state.isGuest)
-            view.guestGroup.goneViews()
-        else
+        view.setupContainer.visibility = View.INVISIBLE
+//        if (state.isGuest)
+//            view.guestGroup.goneViews()
+//        else
             view.guestGroup.showViews()
-        view.signUpPet.visible = true
-        view.loginPet.visible = false
+        view.loginPet.visible = true
+        view.setupPet.visible = false
         view.signUpHeadline.setText(R.string.welcome_hero)
 
     }
@@ -278,41 +344,41 @@ class AuthViewController(args: Bundle? = null) :
     ) {
         val set = AnimatorSet()
         set.playTogether(
-            createContainerRevealAnimation(view.loginContainer, reverse = true),
-            createContainerRevealAnimation(view.signUpContainer)
+            createContainerRevealAnimation(view.setupContainer, reverse = true),
+            createContainerRevealAnimation(view.loginContainer)
         )
         set.interpolator = AccelerateDecelerateInterpolator()
         set.duration = longAnimTime
 
         set.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator?) {
-                view.signUpContainer.visibility = View.VISIBLE
+                view.loginContainer.visibility = View.VISIBLE
                 (view.signUpContent.views() + view.guestGroup.views()).forEach {
                     it.alpha = 0f
                 }
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                view.switchToLogin.visible = true
+//                view.switchToLogin.visible = true
                 (view.signUpContent.views() + view.guestGroup.views()).forEach {
                     it.animate().alpha(1f).start()
                 }
 
                 endAnimation()
 
-                val translateX = view.signUpContainer.width / 2
+                val translateX = view.loginContainer.width / 2
                 val translateY =
-                    view.signUpContainer.height / 2 - ViewUtils.dpToPx(
+                    view.loginContainer.height / 2 - ViewUtils.dpToPx(
                         24f,
                         view.context
                     )
 
-                view.switchToLogin
-                    .animate()
-                    .translationXBy(translateX.toFloat())
-                    .translationYBy(-translateY)
-                    .setDuration(mediumAnimTime)
-                    .start()
+//                view.switchToLogin
+//                    .animate()
+//                    .translationXBy(translateX.toFloat())
+//                    .translationYBy(-translateY)
+//                    .setDuration(mediumAnimTime)
+//                    .start()
 
             }
         })
@@ -321,20 +387,20 @@ class AuthViewController(args: Bundle? = null) :
     }
 
     private fun playShowLoginAnimation(view: View, animationEnd: () -> Unit) {
-        val loginContainer = view.loginContainer
-        val signUpContainer = view.signUpContainer
+        val loginContainer = view.setupContainer
+        val signUpContainer = view.loginContainer
 
-        val translateX = view.signUpContainer.width / 2
+        val translateX = view.loginContainer.width / 2
         val translateY =
-            view.signUpContainer.height / 2 - ViewUtils.dpToPx(24f, view.context)
+            view.loginContainer.height / 2 - ViewUtils.dpToPx(24f, view.context)
 
-        view.switchToLogin
-            .animate()
-            .translationXBy((-translateX).toFloat())
-            .translationYBy(translateY)
-            .setDuration(mediumAnimTime)
-            .withEndAction {
-
+//        view.switchToLogin
+//            .animate()
+//            .translationXBy((-translateX).toFloat())
+//            .translationYBy(translateY)
+//            .setDuration(mediumAnimTime)
+//            .withEndAction {
+//
                 val set = AnimatorSet()
                 set.playTogether(
                     createContainerRevealAnimation(loginContainer),
@@ -345,15 +411,15 @@ class AuthViewController(args: Bundle? = null) :
 
                 set.addListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationStart(animation: Animator?) {
-                        view.switchToLogin.visible = false
+//                        view.switchToLogin.visible = false
                         loginContainer.visibility = View.VISIBLE
-                        view.loginContent.views().forEach {
+                        view.setupContent.views().forEach {
                             it.alpha = 0f
                         }
                     }
 
                     override fun onAnimationEnd(animation: Animator?) {
-                        view.loginContent.views().forEach {
+                        view.setupContent.views().forEach {
                             it.animate().alpha(1f).start()
                         }
                         animationEnd()
@@ -361,8 +427,8 @@ class AuthViewController(args: Bundle? = null) :
                 })
 
                 set.start()
-            }
-            .start()
+//            }
+//            .start()
     }
 
     private fun createContainerRevealAnimation(
@@ -402,12 +468,12 @@ class AuthViewController(args: Bundle? = null) :
             val response = IdpResponse.fromResultIntent(data)
 
             if (resultCode == Activity.RESULT_OK) {
-                dispatch(
-                    AuthAction.CompleteUserAuth(
-                        FirebaseAuth.getInstance().currentUser!!,
-                        view!!.username.text.toString()
-                    )
-                )
+//                dispatch(
+//                    AuthAction.CompleteUserAuth(
+//                        FirebaseAuth.getInstance().currentUser!!,
+//                        view!!.username.text.toString()
+//                    )
+//                )
                 return
             } else {
                 hideLoader()
@@ -434,7 +500,7 @@ class AuthViewController(args: Bundle? = null) :
 
     override fun onAttach(view: View) {
         super.onAttach(view)
-        view.username.clearFocus()
+//        view.username.clearFocus()
         enterFullScreen()
         dispatch(AuthAction.Load)
     }
@@ -442,6 +508,24 @@ class AuthViewController(args: Bundle? = null) :
     override fun onDetach(view: View) {
         exitFullScreen()
         super.onDetach(view)
+    }
+
+    data class AvatarViewModel(
+        val name: String,
+        @DrawableRes val image: Int,
+        @ColorRes val backgroundColor: Int,
+        val avatar: Avatar
+    )
+
+    inner class AvatarAdapter :
+        BaseRecyclerViewAdapter<AvatarViewModel>(R.layout.item_auth_avatar) {
+
+        override fun onBindViewModel(vm: AvatarViewModel, view: View, holder: SimpleViewHolder) {
+            view.avatarName.text = vm.name
+            view.avatarImage.setBackgroundResource(vm.backgroundColor)
+            view.avatarImage.setImageResource(vm.image)
+        }
+
     }
 
 }
