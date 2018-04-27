c77e3bc7cb5f782de1319b2b710085d90cacae03
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-04-27 11:07:35 +0300

    Use reveal change handler

--- app/src/main/java/io/ipoli/android/quest/schedule/calendar/dayview/view/DayViewController.kt
+++ app/src/main/java/io/ipoli/android/quest/schedule/calendar/dayview/view/DayViewController.kt
@@ -32,6 +32,7 @@ import io.ipoli.android.common.datetime.isNotEqual
 import io.ipoli.android.common.datetime.startOfDayUTC
 import io.ipoli.android.common.redux.android.ReduxViewController
 import io.ipoli.android.common.view.*
+import io.ipoli.android.common.view.changehandler.CircularRevealChangeHandler
 import io.ipoli.android.quest.CompletedQuestViewController
 import io.ipoli.android.quest.Icon
 import io.ipoli.android.quest.Reminder
@@ -694,7 +695,21 @@ class DayViewController :
 
                     view.checkBox.visible = true
                     view.setOnClickListener {
-                        showQuest(vm.id)
+//                        showQuest(vm.id)
+                        pushWithRootRouter(
+                            RouterTransaction.with(QuestViewController(vm.id)).tag(
+                                QuestViewController.TAG
+                            ).pushChangeHandler(
+                                CircularRevealChangeHandler(
+                                    cx = 600,
+                                    cy = 800
+                                )
+//                                CircularRevealChangeHandler(
+//                                fromView = view,
+//                                containerView = DayViewController@view
+//                            )
+                        ).popChangeHandler(SimpleSwapChangeHandler())
+                        )
                     }
                 }
 
--- app/src/main/java/io/ipoli/android/repeatingquest/list/RepeatingQuestListViewController.kt
+++ app/src/main/java/io/ipoli/android/repeatingquest/list/RepeatingQuestListViewController.kt
@@ -12,6 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler
+import com.bluelinelabs.conductor.changehandler.SimpleSwapChangeHandler
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.IIcon
 import com.mikepenz.ionicons_typeface_library.Ionicons
@@ -21,6 +22,7 @@ import io.ipoli.android.common.redux.android.ReduxViewController
 import io.ipoli.android.common.text.DateFormatter
 import io.ipoli.android.common.text.DurationFormatter
 import io.ipoli.android.common.view.*
+import io.ipoli.android.common.view.changehandler.CircularRevealChangeHandler
 import io.ipoli.android.common.view.recyclerview.BaseRecyclerViewAdapter
 import io.ipoli.android.common.view.recyclerview.SimpleViewHolder
 import io.ipoli.android.repeatingquest.add.AddRepeatingQuestViewController
@@ -160,10 +162,20 @@ class RepeatingQuestListViewController(args: Bundle? = null) :
 
             view.setOnClickListener {
                 val changeHandler = FadeChangeHandler()
-                rootRouter.pushController(
+//                rootRouter.pushController(
+//                    RouterTransaction.with(RepeatingQuestViewController(vm.id))
+//                        .pushChangeHandler(changeHandler)
+//                        .popChangeHandler(changeHandler)
+//                )
+
+                pushWithRootRouter(
                     RouterTransaction.with(RepeatingQuestViewController(vm.id))
-                        .pushChangeHandler(changeHandler)
-                        .popChangeHandler(changeHandler)
+                        .pushChangeHandler(
+                            CircularRevealChangeHandler(
+                                fromView = view,
+                                containerView = RepeatingQuestViewController@ view
+                            )
+                        ).popChangeHandler(SimpleSwapChangeHandler())
                 )
             }
         }
