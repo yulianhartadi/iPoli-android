9a982f7ffa312c42cd06ec892c7834dd4cc48a7a
Venelin Valkov <venelin@curiousily.com>
2018-06-30 19:16:47 +0300

    Update widget & quickDo notification on date changed

--- app/src/main/java/io/ipoli/android/common/DateChangedReceiver.kt
+++ app/src/main/java/io/ipoli/android/common/DateChangedReceiver.kt
@@ -8,22 +8,21 @@ import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import org.threeten.bp.LocalDate
 import space.traversal.kapsule.required
+import timber.log.Timber
 
 /**
  * Created by Venelin Valkov <venelin@mypoli.fun>
- * on 03/03/2018.
+ * on 06/30/2018.
  */
-class BootCompleteReceiver : AsyncBroadcastReceiver() {
+class DateChangedReceiver : AsyncBroadcastReceiver() {
 
     private val playerRepository by required { playerRepository }
     private val questRepository by required { questRepository }
-    private val reminderScheduler by required { reminderScheduler }
 
     override suspend fun onReceiveAsync(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_DATE_CHANGED) {
             val p = playerRepository.find()
             if (p != null) {
-                reminderScheduler.schedule()
                 val quests = questRepository.findScheduledAt(LocalDate.now())
                 launch(UI) {
                     AppWidgetUtil.updateAgendaWidget(context)
