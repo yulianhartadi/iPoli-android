f13c37ec1ce648bda87d908f1199b5f31475d84d
Venelin Valkov <venelin@curiousily.com>
2018-07-29 10:05:20 +0300

    Cleanup UseCase

--- app/src/main/java/io/ipoli/android/repeatingquest/usecase/FindNextDateForRepeatingQuestUseCase.kt
+++ app/src/main/java/io/ipoli/android/repeatingquest/usecase/FindNextDateForRepeatingQuestUseCase.kt
@@ -1,7 +1,6 @@
 package io.ipoli.android.repeatingquest.usecase
 
 import io.ipoli.android.common.UseCase
-import io.ipoli.android.common.datetime.isBeforeOrEqual
 import io.ipoli.android.quest.RepeatingQuest
 import io.ipoli.android.quest.data.persistence.QuestRepository
 import org.threeten.bp.LocalDate
@@ -20,78 +19,10 @@ class FindNextDateForRepeatingQuestUseCase(
         val nextScheduled =
             questRepository.findNextScheduledNotCompletedForRepeatingQuest(rq.id, fromDate)
 
-        if (nextScheduled != null) {
-
-            if (fromDate.isBeforeOrEqual(nextScheduled.scheduledDate!!)) {
-                return rq.copy(
-                    nextDate = nextScheduled.scheduledDate
-                )
-            }
-
-            val patternNext = rq.repeatPattern.nextDate(fromDate)
-            if (patternNext == null) {
-                return rq.copy(
-                    nextDate = nextScheduled.scheduledDate
-                )
-            }
-
-            if (patternNext.isAfter(nextScheduled.scheduledDate) || patternNext.isEqual(
-                    nextScheduled.scheduledDate
-                )
-            ) {
-                return rq.copy(
-                    nextDate = nextScheduled.scheduledDate
-                )
-            }
-
-            var nextDate = patternNext
-
-            while (nextDate!!.isBefore(nextScheduled.scheduledDate)) {
-
-                val originalScheduled =
-                    questRepository.findOriginalScheduledForRepeatingQuestAtDate(rq.id, nextDate)
-                if (originalScheduled == null) {
-                    return rq.copy(
-                        nextDate = nextDate
-                    )
-                }
-                nextDate = rq.repeatPattern.nextDate(nextDate.plusDays(1))
-
-                if (nextDate == null) {
-                    return rq.copy(
-                        nextDate = nextScheduled.scheduledDate
-                    )
-                }
-            }
-
-            return rq.copy(
-                nextDate = nextScheduled.scheduledDate
-            )
-        } else {
-            var nextDate: LocalDate? = rq.repeatPattern.nextDate(fromDate)
-            if (nextDate == null) {
-                return rq.copy(
-                    nextDate = null
-                )
-            }
-            while (true) {
-
-                val originalScheduled =
-                    questRepository.findOriginalScheduledForRepeatingQuestAtDate(rq.id, nextDate!!)
-                if (originalScheduled == null) {
-                    return rq.copy(
-                        nextDate = nextDate
-                    )
-                }
-                nextDate = rq.repeatPattern.nextDate(nextDate.plusDays(1))
-
-                if (nextDate == null) {
-                    return rq.copy(
-                        nextDate = null
-                    )
-                }
-            }
-        }
+        return rq.copy(
+            nextDate = nextScheduled?.let {
+                it.scheduledDate!!
+            })
     }
 
     data class Params(val repeatingQuest: RepeatingQuest, val fromDate: LocalDate = LocalDate.now())
