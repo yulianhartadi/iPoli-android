44da8c064a4765e5875734d3447c190f4fda9e93
Venelin Valkov <venelin@curiousily.com>
2018-10-25 09:37:14 +0300

    Make agenda default screen

--- app/src/main/java/io/ipoli/android/quest/schedule/ScheduleViewController.kt
+++ app/src/main/java/io/ipoli/android/quest/schedule/ScheduleViewController.kt
@@ -12,7 +12,7 @@ import io.ipoli.android.common.redux.android.ReduxViewController
 import io.ipoli.android.common.view.*
 import io.ipoli.android.quest.schedule.ScheduleViewState.StateType.*
 import io.ipoli.android.quest.schedule.addquest.AddQuestAnimationHelper
-import io.ipoli.android.quest.schedule.calendar.CalendarViewController
+import io.ipoli.android.quest.schedule.agenda.AgendaViewController
 import kotlinx.android.synthetic.main.controller_schedule.view.*
 import kotlinx.android.synthetic.main.view_calendar_toolbar.view.*
 import org.threeten.bp.LocalDate
@@ -65,7 +65,7 @@ class ScheduleViewController(args: Bundle? = null) :
 
         setChildController(
             view.contentContainer,
-            CalendarViewController(currentDate)
+            AgendaViewController(currentDate)
         )
         return view
     }
